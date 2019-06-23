5bd52a0b3fddd9b78f1fc21694636e2a186f01f5
haessal <haessal@mizutamauki.net>
2019-06-23 17:53:10 +0900

    Enable Email Verification

--- book-keeping/app/User.php
+++ book-keeping/app/User.php
@@ -2,10 +2,11 @@
 
 namespace App;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Notifiable;
 
