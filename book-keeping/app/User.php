be475147347919809e1dace3db61aa9c1c719ca2
haessal <haessal@mizutamauki.net>
2019-07-09 21:28:50 +0900

    Remove email address from link URI for password reset

--- book-keeping/app/User.php
+++ book-keeping/app/User.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Lang;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -36,4 +39,25 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param string $token
+     *
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        ResetPasswordNotification::toMailUsing(function ($notifiable, $token) {
+            return (new MailMessage())
+                ->subject(Lang::getFromJson('Reset Password Notification'))
+                ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
+                ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', ['token' => $token], false)))
+                ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]))
+                ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+        });
+
+        $this->notify((new ResetPasswordNotification($token)));
+    }
 }
