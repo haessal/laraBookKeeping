464872228141bf293251d254834c406abee141ef
haessal <haessal@mizutamauki.net>
2019-09-14 16:04:45 +0900

    Make files for DB migration
    command:
    php artisan make:migration create_books_table --create bk2_0_books
    php artisan make:migration create_permissions_table --create bk2_0_permissions
    php artisan make:migration create_account_groups_table --create bk2_0_account_groups
    php artisan make:migration create_accounts_table --create bk2_0_accounts
    php artisan make:migration create_budgets_table --create bk2_0_budgets
    php artisan make:migration create_slips_table --create bk2_0_slips
    php artisan make:migration create_slip_entries_table --create bk2_0_slip_entries
    php artisan make:migration create_slip_groups_table --create bk2_0_slip_groups
    php artisan make:migration create_slip_group_entries_table --create bk2_0_slip_group_entries

---
+++ book-keeping/database/migrations/2019_09_09_174722_create_slip_group_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlipGroupEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bk2_0_slip_group_entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bk2_0_slip_group_entries');
+    }
+}
