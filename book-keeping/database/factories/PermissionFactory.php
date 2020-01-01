3845f8b23e4091162f437af6b7977e175fe5504f
haessal <haessal@mizutamauki.net>
2020-01-01 09:39:19 +0000

    Test PermissionRepository

---
+++ book-keeping/database/factories/PermissionFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\DataProvider\Eloquent\Permission;
+use Faker\Generator as Faker;
+
+$factory->define(Permission::class, function (Faker $faker) {
+    return [
+    ];
+});
