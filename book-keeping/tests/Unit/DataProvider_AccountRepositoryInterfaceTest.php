795145d48001822135493498d2c0564d7cf1dda8
haessal <haessal@mizutamauki.net>
2020-01-04 12:52:48 +0000

    Test AccountRepository in case the function is called with null as the argument

--- book-keeping/tests/Unit/DataProvider_AccountRepositoryInterfaceTest.php
+++ book-keeping/tests/Unit/DataProvider_AccountRepositoryInterfaceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 abstract class DataProvider_AccountRepositoryInterfaceTest extends TestCase
@@ -12,7 +13,7 @@ abstract class DataProvider_AccountRepositoryInterfaceTest extends TestCase
      */
     public function create_ReturnValueTypeIsString()
     {
-        $accountGroupId = '0000';
+        $accountGroupId = (string) Str::uuid();
         $title = 'string';
         $description = 'string';
         $bk_uid = 0;
@@ -28,9 +29,25 @@ abstract class DataProvider_AccountRepositoryInterfaceTest extends TestCase
     /**
      * @test
      */
+    public function create_CalledWithNullForNullable()
+    {
+        $accountGroupId = (string) Str::uuid();
+        $title = 'string';
+        $description = 'string';
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $accountId = $this->account->create($accountGroupId, $title, $description, null, null);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->assertTrue(is_string($accountId));
+    }
+
+    /**
+     * @test
+     */
     public function searchAccount_ReturnValueTypeIsArray()
     {
-        $bookId = '0000';
+        $bookId = (string) Str::uuid();
 
         $accountList = $this->account->searchAccount($bookId);
 
