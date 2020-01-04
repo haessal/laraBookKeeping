6f489d7fc12d9c4341066cd890b88393b47d4fea
haessal <haessal@mizutamauki.net>
2020-01-04 12:58:17 +0000

    Test SlipRepository in case the function is called with null as the argument

--- book-keeping/tests/Unit/DataProvider_SlipRepositoryInterfaceTest.php
+++ book-keeping/tests/Unit/DataProvider_SlipRepositoryInterfaceTest.php
@@ -25,4 +25,21 @@ abstract class DataProvider_SlipRepositoryInterfaceTest extends TestCase
 
         $this->assertTrue(is_string($slipId));
     }
+
+    /**
+     * @test
+     */
+    public function create_CalledWithNullForNullable()
+    {
+        $bookId = (string) Str::uuid();
+        $outline = 'outline2';
+        $date = '2019-07-02';
+        $isDraft = true;
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $slipId = $this->slip->create($bookId, $outline, $date, null, $isDraft);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->assertTrue(is_string($slipId));
+    }
 }
