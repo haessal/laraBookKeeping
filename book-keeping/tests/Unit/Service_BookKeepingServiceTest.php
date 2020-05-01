e11c79c947bbfc09e4e0d9316dc95b795492695f
haessal <haessal@mizutamauki.net>
2020-05-02 07:38:44 +0900

    Add cases to Test BookKeepingService

--- book-keeping/tests/Unit/Service_BookKeepingServiceTest.php
+++ book-keeping/tests/Unit/Service_BookKeepingServiceTest.php
@@ -534,6 +534,102 @@ class Service_BookKeepingServiceTest extends TestCase
     /**
      * @test
      */
+    public function retrieveAccountsList_RetrieveTheDefaultBookAccounts()
+    {
+        $bookId = (string) Str::uuid();
+        $userId = 192;
+        $user = new User();
+        $user->id = $userId;
+        $this->be($user);
+        $accountId_1 = (string) Str::uuid();
+        $accountGroupId_1 = (string) Str::uuid();
+        $accounts = [
+            $accountId_1 => [
+                'account_type'             => AccountService::ACCOUNT_TYPE_ASSET,
+                'account_group_id'         => $accountGroupId_1,
+                'account_group_title'      => 'accountGroupTitle_1',
+                'is_current'               => 0,
+                'account_id'               => $accountId_1,
+                'account_title'            => 'accountTitle_1',
+                'description'              => 'description_1',
+                'selectable'               => 1,
+                'account_bk_code'          => 1201,
+                'created_at'               => '2019-12-03 12:00:01',
+                'account_group_bk_code'    => 1200,
+                'account_group_created_at' => '2019-12-04 12:00:12',
+            ],
+        ];
+        $accounts_expected = $accounts;
+        /** @var \App\Service\BookService|\Mockery\MockInterface $bookMock */
+        $bookMock = Mockery::mock(BookService::class);
+        $bookMock->shouldReceive('retrieveDefaultBook')
+            ->once()
+            ->with($userId)
+            ->andReturn($bookId);
+        /** @var \App\Service\AccountService|\Mockery\MockInterface $accountMock */
+        $accountMock = Mockery::mock(AccountService::class);
+        $accountMock->shouldReceive('retrieveAccounts')
+            ->once()
+            ->with($bookId)
+            ->andReturn($accounts);
+        /** @var \App\Service\BudgetService|\Mockery\MockInterface $budgetMock */
+        $budgetMock = Mockery::mock(BudgetService::class);
+        /** @var \App\Service\SlipService|\Mockery\MockInterface $slipMock */
+        $slipMock = Mockery::mock(SlipService::class);
+
+        $BookKeeping = new BookKeepingService($bookMock, $accountMock, $budgetMock, $slipMock);
+        $accounts_actual = $BookKeeping->retrieveAccountsList();
+
+        $this->assertSame($accounts_expected, $accounts_actual);
+    }
+
+    /**
+     * @test
+     */
+    public function retrieveAccountsList_RetrieveTheSpecifiedBookAccounts()
+    {
+        $bookId = (string) Str::uuid();
+        $accountId_1 = (string) Str::uuid();
+        $accountGroupId_1 = (string) Str::uuid();
+        $accounts = [
+            $accountId_1 => [
+                'account_type'             => AccountService::ACCOUNT_TYPE_ASSET,
+                'account_group_id'         => $accountGroupId_1,
+                'account_group_title'      => 'accountGroupTitle_1',
+                'is_current'               => 0,
+                'account_id'               => $accountId_1,
+                'account_title'            => 'accountTitle_1',
+                'description'              => 'description_1',
+                'selectable'               => 1,
+                'account_bk_code'          => 1201,
+                'created_at'               => '2019-12-03 12:00:01',
+                'account_group_bk_code'    => 1200,
+                'account_group_created_at' => '2019-12-04 12:00:12',
+            ],
+        ];
+        $accounts_expected = $accounts;
+        /** @var \App\Service\BookService|\Mockery\MockInterface $bookMock */
+        $bookMock = Mockery::mock(BookService::class);
+        /** @var \App\Service\AccountService|\Mockery\MockInterface $accountMock */
+        $accountMock = Mockery::mock(AccountService::class);
+        $accountMock->shouldReceive('retrieveAccounts')
+            ->once()
+            ->with($bookId)
+            ->andReturn($accounts);
+        /** @var \App\Service\BudgetService|\Mockery\MockInterface $budgetMock */
+        $budgetMock = Mockery::mock(BudgetService::class);
+        /** @var \App\Service\SlipService|\Mockery\MockInterface $slipMock */
+        $slipMock = Mockery::mock(SlipService::class);
+
+        $BookKeeping = new BookKeepingService($bookMock, $accountMock, $budgetMock, $slipMock);
+        $accounts_actual = $BookKeeping->retrieveAccountsList($bookId);
+
+        $this->assertSame($accounts_expected, $accounts_actual);
+    }
+
+    /**
+     * @test
+     */
     public function retrieveDraftSlips_RetrieveTheDefaultBookDraftSlips()
     {
         $bookId = (string) Str::uuid();
