6a01509be479dbc5ece833ca95ae545c05e0686d
haessal <haessal@mizutamauki.net>
2020-05-03 23:32:06 +0900

    WIP [30] Add version 1 API

--- book-keeping/app/Http/Controllers/api/v1/PostSlipsActionApi.php
+++ book-keeping/app/Http/Controllers/api/v1/PostSlipsActionApi.php
@@ -41,9 +41,10 @@ class PostSlipsActionApi extends AuthenticatedBookKeepingActionApi
     public function __invoke(Request $request): JsonResponse
     {
         $context = [];
-        $slip = $this->trimDraftSlip($request->all());
         $accounts = $this->BookKeeping->retrieveAccountsList();
-        if ($this->validateDraftSlip($slip, $accounts)) {
+        $result = $this->validateAndTrimDraftSlip($request->all(), $accounts);
+        if ($result['success']) {
+            $slip = $result['slip'];
             $slipId = $this->BookKeeping->createSlip($slip['outline'], $slip['date'], $slip['entries'], $slip['memo']);
             $context['slips'] = $this->BookKeeping->retrieveSlip($slipId);
             $response = $this->responder->response($context);
@@ -64,38 +65,42 @@ class PostSlipsActionApi extends AuthenticatedBookKeepingActionApi
     private function trimDraftSlip(array $slip_in): array
     {
         $slip_out = [];
-        if (array_key_exists('outline', $slip_in)) {
+        if (array_key_exists('outline', $slip_in) && is_string($slip_in['outline'])) {
             $slip_out['outline'] = trim($slip_in['outline']);
         }
-        if (array_key_exists('date', $slip_in)) {
+        if (array_key_exists('date', $slip_in) && is_string($slip_in['date'])) {
             $slip_out['date'] = trim($slip_in['date']);
         }
         $slip_out['entries'] = [];
         if (array_key_exists('entries', $slip_in)) {
-            if (!empty($slip_in['entries'])) {
+            if (!empty($slip_in['entries']) && is_array($slip_in['entries'])) {
                 foreach ($slip_in['entries'] as $slipEntry_in) {
-                    $slipEntry_out = [];
-                    if (array_key_exists('debit', $slipEntry_in)) {
-                        $slipEntry_out['debit'] = trim($slipEntry_in['debit']);
+                    if (!empty($slipEntry_in) && is_array($slipEntry_in)) {
+                        $slipEntry_out = [];
+                        if (array_key_exists('debit', $slipEntry_in) && is_string($slipEntry_in['debit'])) {
+                            $slipEntry_out['debit'] = trim($slipEntry_in['debit']);
+                        }
+                        if (array_key_exists('credit', $slipEntry_in) && is_string($slipEntry_in['credit'])) {
+                            $slipEntry_out['credit'] = trim($slipEntry_in['credit']);
+                        }
+                        if (array_key_exists('amount', $slipEntry_in)) {
+                            $slipEntry_out['amount'] = $slipEntry_in['amount'];
+                        }
+                        if (array_key_exists('client', $slipEntry_in) && is_string($slipEntry_in['client'])) {
+                            $slipEntry_out['client'] = trim($slipEntry_in['client']);
+                        }
+                        if (array_key_exists('outline', $slipEntry_in) && is_string($slipEntry_in['outline'])) {
+                            $slipEntry_out['outline'] = trim($slipEntry_in['outline']);
+                        }
+                        if (!empty($slipEntry_out)) {
+                            $slip_out['entries'][] = $slipEntry_out;
+                        }
                     }
-                    if (array_key_exists('credit', $slipEntry_in)) {
-                        $slipEntry_out['credit'] = trim($slipEntry_in['credit']);
-                    }
-                    if (array_key_exists('amount', $slipEntry_in)) {
-                        $slipEntry_out['amount'] = $slipEntry_in['amount'];
-                    }
-                    if (array_key_exists('client', $slipEntry_in)) {
-                        $slipEntry_out['client'] = trim($slipEntry_in['client']);
-                    }
-                    if (array_key_exists('outline', $slipEntry_in)) {
-                        $slipEntry_out['outline'] = trim($slipEntry_in['outline']);
-                    }
-                    $slip_out['entries'][] = $slipEntry_out;
                 }
             }
         }
         $slip_out['memo'] = null;
-        if (array_key_exists('memo', $slip_in)) {
+        if (array_key_exists('memo', $slip_in) && is_string($slip_in['memo'])) {
             $trim_memo = trim($slip_in['memo']);
             if (!empty($trim_memo)) {
                 $slip_out['memo'] = $trim_memo;
@@ -106,49 +111,97 @@ class PostSlipsActionApi extends AuthenticatedBookKeepingActionApi
     }
 
     /**
-     * Validate draft slip.
+     * Validate draft slip and trim string data.
      *
      * @param array $slip
      * @param array $accounts
      *
-     * @return bool
+     * @return array
      */
-    private function validateDraftSlip(array $slip, array $accounts): bool
+    private function validateAndTrimDraftSlip(array $slip_in, array $accounts): array
     {
         $success = true;
+        $slip_out = [];
 
-        if (!array_key_exists('outline', $slip) || empty($slip['outline'])) {
-            $success = false;
-        }
-        if (!array_key_exists('date', $slip) || empty($slip['date'])) {
+        if (!array_key_exists('outline', $slip_in) || empty($slip_in['outline']) || !is_string($slip_in['outline'])) {
             $success = false;
         } else {
-            if (!($this->BookKeeping->validateDateFormat($slip['date']))) {
+            $slip_out['outline'] = trim($slip_in['outline']);
+        }
+        if (!array_key_exists('date', $slip_in) || empty($slip_in['date']) || !is_string($slip_in['date'])) {
+            $success = false;
+        } else {
+            $trim_date = trim($slip_in['date']);
+            if (!($this->BookKeeping->validateDateFormat($trim_date))) {
                 $success = false;
+            } else {
+                $slip_out['date'] = $trim_date;
             }
         }
-        if (!array_key_exists('entries', $slip) || empty($slip['entries']) || !is_array($slip['entries'])) {
+        if (!array_key_exists('entries', $slip_in) || empty($slip_in['entries'])) {
             $success = false;
         } else {
-            foreach ($slip['entries'] as $slipEntry) {
-                if (!array_key_exists('debit', $slipEntry) || empty($slipEntry['debit']) || !array_key_exists($slipEntry['debit'], $accounts)) {
-                    $success = false;
+            if (!is_array($slip_in['entries'])) {
+                $success = false;
+            } else {
+                $slip_out['entries'] = [];
+                foreach ($slip_in['entries'] as $slipEntry_in) {
+                    if (empty($slipEntry_in) || !is_array($slipEntry_in)) {
+                        $success = false;
+                    } else {
+                        $slipEntry_out = [];
+                        if (!array_key_exists('debit', $slipEntry_in) || empty($slipEntry_in['debit']) || !is_string($slipEntry_in['debit'])) {
+                            $success = false;
+                        } else {
+                            $trim_debit = trim($slipEntry_in['debit']);
+                            if (!array_key_exists($trim_debit, $accounts)) {
+                                $success = false;
+                            } else {
+                                $slipEntry_out['debit'] = $trim_debit;
+                            }
+                        }
+                        if (!array_key_exists('credit', $slipEntry_in) || empty($slipEntry_in['credit']) || !is_string($slipEntry_in['credit'])) {
+                            $success = false;
+                        } else {
+                            $trim_credit = trim($slipEntry_in['credit']);
+                            if (!array_key_exists($trim_credit, $accounts)) {
+                                $success = false;
+                            } else {
+                                $slipEntry_out['credit'] = $trim_credit;
+                            }
+                        }
+                        if (!array_key_exists('amount', $slipEntry_in) || empty($slipEntry_in['amount']) || !is_numeric($slipEntry_in['amount'])) {
+                            $success = false;
+                        } else {
+                            $slipEntry_out['amount'] = $slipEntry_in['amount'];
+                        }
+                        if (!array_key_exists('client', $slipEntry_in) || empty($slipEntry_in['client']) || !is_string($slipEntry_in['client'])) {
+                            $success = false;
+                        } else {
+                            $slipEntry_out['client'] = trim($slipEntry_in['client']);
+                        }
+                        if (!array_key_exists('outline', $slipEntry_in) || empty($slipEntry_in['outline']) || !is_string($slipEntry_in['outline'])) {
+                            $success = false;
+                        } else {
+                            $slipEntry_out['outline'] = trim($slipEntry_in['outline']);
+                        }
+                        if (!empty($slipEntry_out)) {
+                            $slip_out['entries'][] = $slipEntry_out;
+                        }
+                    }
                 }
-                if (!array_key_exists('credit', $slipEntry) || empty($slipEntry['credit']) || !array_key_exists($slipEntry['credit'], $accounts)) {
-                    $success = false;
-                }
-                if (!array_key_exists('amount', $slipEntry) || empty($slipEntry['amount']) || !is_numeric($slipEntry['amount'])) {
-                    $success = false;
-                }
-                if (!array_key_exists('client', $slipEntry) || empty($slipEntry['client'])) {
-                    $success = false;
-                }
-                if (!array_key_exists('outline', $slipEntry) || empty($slipEntry['outline'])) {
-                    $success = false;
-                }
+            }
+        }
+        if (!array_key_exists('memo', $slip_in) || empty($slip_in['memo'])) {
+            $slip_out['memo'] = null;
+        } else {
+            if (!is_string($slip_in['memo'])) {
+                $success = false;
+            } else {
+                $slip_out['memo'] = $slip_in['memo'];
             }
         }
 
-        return $success;
+        return ['success' => $success, 'slip' => $slip_out];
     }
 }
