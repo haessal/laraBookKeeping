6dffc98de8a43884ba3f4beb3595921ab14fddcc
haessal <haessal@mizutamauki.net>
2020-05-04 12:54:18 +0900

    WIP [38] Add version 1 API (Test)

--- book-keeping/tests/Unit/Http_Controllers_api_v1_PostSlipsActionApiTest.php
+++ book-keeping/tests/Unit/Http_Controllers_api_v1_PostSlipsActionApiTest.php
@@ -28,9 +28,9 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
 
     /**
      * @test
-     * @dataProvider forTestTrimDraftSlip
+     * @dataProvider forValidateAndTrimString
      */
-    public function trimDraftSlip_($slip_in, $slip_expected)
+    public function validateAndTrimString($array_in, $key, $string_expected)
     {
         /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
         $BookKeepingMock = Mockery::mock(BookKeepingService::class);
@@ -39,11 +39,98 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
 
         $controller = new PostSlipsActionApi($BookKeepingMock, $responderMock);
         $reflection = new ReflectionClass($controller);
-        $method = $reflection->getMethod('trimDraftSlip');
+        $method = $reflection->getMethod('validateAndTrimString');
         $method->setAccessible(true);
-        $slip_actual = $method->invoke($controller, $slip_in);
+        $string_actual = $method->invoke($controller, $array_in, $key);
 
-        $this->assertSame($slip_expected, $slip_actual);
+        $this->assertSame($string_expected, $string_actual);
+    }
+
+    public function forValidateAndTrimString()
+    {
+        return [
+            [[], 'outline', null,],
+            [['outline' => ['  outline1']], 'outline', null,],
+            [['outline' => '  '], 'outline', null,],
+            [['outline' => '  outline1'], 'outline', 'outline1',],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider forValidateAndTrimAccounts
+     */
+    public function validateAndTrimAccounts($array_in, $key, $accounts, $string_expected)
+    {
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        /** @var \App\Http\Responder\api\v1\SlipJsonResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(SlipJsonResponder::class);
+
+        $controller = new PostSlipsActionApi($BookKeepingMock, $responderMock);
+        $reflection = new ReflectionClass($controller);
+        $method = $reflection->getMethod('validateAndTrimAccounts');
+        $method->setAccessible(true);
+        $string_actual = $method->invoke($controller, $array_in, $key, $accounts);
+
+        $this->assertSame($string_expected, $string_actual);
+
+    }
+
+    public function forValidateAndTrimAccounts()
+    {
+        return [
+            [
+                ['debit' => 0],
+                'debit',
+                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null,],
+                null,
+            ],
+            [
+                ['debit' => '  471b26d0-99a1-47f4-aa57-2722f6011f2a'],
+                'debit',
+                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null,],
+                null,
+            ],
+            [
+                ['debit' => '3274cc74-f7ab-40a4-984a-186a593401f7  '],
+                'debit',
+                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null,],
+                '3274cc74-f7ab-40a4-984a-186a593401f7',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider forTestTrimDraftSlip
+     */
+    public function validateAndTrimDraftSlip_($slip_in, $result_expected, $callValidateDateFormat, $validateDateFormatResult)
+    {
+        $accounts = [
+            '3274cc74-f7ab-40a4-984a-186a593401f7' => null,
+            '471b26d0-99a1-47f4-aa57-2722f6011f2a' => null,
+            '8548f227-7be9-42be-b5c7-66da432f5dad' => null,
+            '90dc7df5-07ea-4086-9461-0555c2a9d03c' => null,
+        ];
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        if ($callValidateDateFormat) {
+            $BookKeepingMock->shouldReceive('validateDateFormat')
+                ->once()
+                ->with(trim($slip_in['date']))
+                ->andReturn($validateDateFormatResult);
+        }
+        /** @var \App\Http\Responder\api\v1\SlipJsonResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(SlipJsonResponder::class);
+
+        $controller = new PostSlipsActionApi($BookKeepingMock, $responderMock);
+        $reflection = new ReflectionClass($controller);
+        $method = $reflection->getMethod('validateAndTrimDraftSlip');
+        $method->setAccessible(true);
+        $result_actual = $method->invoke($controller, $slip_in, $accounts);
+
+        $this->assertSame($result_expected, $result_actual);
     }
 
     public function forTestTrimDraftSlip()
@@ -51,7 +138,64 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
         return [
             [
                 [],
-                ['entries' => [], 'memo' => null],
+                [
+                    'success' => false,
+                    'slip' => ['memo' => null],
+                ],
+                false,
+                false,
+            ],
+            /*
+            [
+                ['outline' => ['  outline1']],
+                [
+                    'success' => false,
+                    'slip' => ['memo' => null],
+                ],
+                false,
+                false,
+            ],
+            [
+                ['outline' => '  '],
+                [
+                    'success' => false,
+                    'slip' => ['memo' => null],
+                ],
+                false,
+                false,
+            ],
+            [
+                ['outline' => '  outline1'],
+                [
+                    'success' => false,
+                    'slip' => ['outline' => 'outline1',  'memo' => null],
+                ],
+                false,
+                false,
+            ],
+            [
+                ['outline' => ''],
+                [
+                    'success' => false,
+                    'slip' => ['memo' => null],
+                ],
+                false,
+                false,
+            ],
+            [
+                ['outline' => '  '],
+                [
+                    'success' => false,
+                    'slip' => ['memo' => null],
+                ],
+                false,
+                false,
+            ],
+            [
+                ['date' => '2020-01-01'],
+                ['success' => false, 'slip' => ['date' => '2020-01-01', 'memo' => null]],
+                true,
+                true,
             ],
             [
                 ['outline' => '  outline1'],
@@ -211,6 +355,7 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
                     'memo' => 'memo11',
                 ],
             ],
+            */
         ];
     }
 }
