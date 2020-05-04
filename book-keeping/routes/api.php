8a5180bd573ab224a690f206294c606dbf1e2353
haessal <haessal@mizutamauki.net>
2020-05-04 21:57:28 +0900

    Support API: POST /v1/slips

--- book-keeping/routes/api.php
+++ book-keeping/routes/api.php
@@ -18,3 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/v1/accounts', api\v1\GetAccountsActionApi::class);
+
+Route::post('/v1/slips', api\v1\PostSlipsActionApi::class);
