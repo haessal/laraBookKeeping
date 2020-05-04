459adca3529eb0797195d9460b9f3d2efd2c404f
haessal <haessal@mizutamauki.net>
2020-05-04 14:15:23 +0900

    WIP [41] Add version 1 API (Test)

--- book-keeping/tests/Unit/Http_Controllers_api_v1_PostSlipsActionApiTest.php
+++ book-keeping/tests/Unit/Http_Controllers_api_v1_PostSlipsActionApiTest.php
@@ -60,8 +60,11 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
      * @test
      * @dataProvider forValidateAndTrimAccounts
      */
-    public function validateAndTrimAccounts($array_in, $key, $accounts, $string_expected)
+    public function validateAndTrimAccounts($array_in, $key, $string_expected)
     {
+        $accounts = [
+            '3274cc74-f7ab-40a4-984a-186a593401f7' => null,
+        ];
         /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
         $BookKeepingMock = Mockery::mock(BookKeepingService::class);
         /** @var \App\Http\Responder\api\v1\SlipJsonResponder|\Mockery\MockInterface $responderMock */
@@ -79,23 +82,197 @@ class Http_Controllers_api_v1_PostSlipsActionApiTest extends TestCase
     public function forValidateAndTrimAccounts()
     {
         return [
+            [['debit' => 0], 'debit', null],
+            [['debit' => '  471b26d0-99a1-47f4-aa57-2722f6011f2a'], 'debit', null],
+            [['debit' => '3274cc74-f7ab-40a4-984a-186a593401f7  '], 'debit', '3274cc74-f7ab-40a4-984a-186a593401f7'],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider forValidateAndTrimDraftSlipEntry
+     */
+    public function validateAndTrimDraftSlipEntry($slipEntry_in, $result_expected)
+    {
+        $accounts = [
+            '3274cc74-f7ab-40a4-984a-186a593401f7' => null,
+            '471b26d0-99a1-47f4-aa57-2722f6011f2a' => null,
+        ];
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        /** @var \App\Http\Responder\api\v1\SlipJsonResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(SlipJsonResponder::class);
+
+        $controller = new PostSlipsActionApi($BookKeepingMock, $responderMock);
+        $reflection = new ReflectionClass($controller);
+        $method = $reflection->getMethod('validateAndTrimDraftSlipEntry');
+        $method->setAccessible(true);
+        $result_actual = $method->invoke($controller, $slipEntry_in, $accounts);
+
+        $this->assertSame($result_expected, $result_actual);
+    }
+
+    public function forValidateAndTrimDraftSlipEntry()
+    {
+        return [
             [
-                ['debit' => 0],
-                'debit',
-                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null],
-                null,
+                [
+                    'debit'   => '8548f227-7be9-42be-b5c7-66da432f5dad',
+                    'credit'  => '90dc7df5-07ea-4086-9461-0555c2a9d03c',
+                    'client'  => ['client1'],
+                    'outline' => ['outline1'],
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [],
+                ],
             ],
             [
-                ['debit' => '  471b26d0-99a1-47f4-aa57-2722f6011f2a'],
-                'debit',
-                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null],
-                null,
+                [
+                    'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7  ',
+                    'credit'  => '90dc7df5-07ea-4086-9461-0555c2a9d03c',
+                    'client'  => ['client1'],
+                    'outline' => ['outline1'],
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'debit' => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    ],
+                ],
             ],
             [
-                ['debit' => '3274cc74-f7ab-40a4-984a-186a593401f7  '],
-                'debit',
-                ['3274cc74-f7ab-40a4-984a-186a593401f7' => null],
-                '3274cc74-f7ab-40a4-984a-186a593401f7',
+                [
+                    'debit'   => '8548f227-7be9-42be-b5c7-66da432f5dad',
+                    'credit'  => '  471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                    'client'  => ['client1'],
+                    'outline' => ['outline1'],
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'credit' => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '8548f227-7be9-42be-b5c7-66da432f5dad',
+                    'credit'  => '90dc7df5-07ea-4086-9461-0555c2a9d03c',
+                    'client'  => '  client1',
+                    'outline' => ['outline1'],
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'client' => 'client1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '8548f227-7be9-42be-b5c7-66da432f5dad',
+                    'credit'  => '90dc7df5-07ea-4086-9461-0555c2a9d03c',
+                    'client'  => ['client1'],
+                    'outline' => 'outline1  ',
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'outline' => 'outline1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '  3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a  ',
+                    'amount'  => 200,
+                    'client'  => ' client1 ',
+                    'outline' => '  outline1  ',
+                ],
+                [
+                    'success'    => true,
+                    'slip_entry' => [
+                        'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                        'amount'  => 200,
+                        'client'  => 'client1',
+                        'outline' => 'outline1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'credit'  => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'amount'  => 200,
+                    'client'  => 'client1',
+                    'outline' => 'outline1',
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'credit'  => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'amount'  => 200,
+                        'client'  => 'client1',
+                        'outline' => 'outline1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                    'client'  => 'client1',
+                    'outline' => 'outline1',
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                        'client'  => 'client1',
+                        'outline' => 'outline1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                    'amount'  => 0,
+                    'client'  => 'client1',
+                    'outline' => 'outline1',
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                        'client'  => 'client1',
+                        'outline' => 'outline1',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                    'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                    'amount'  => '123',
+                    'client'  => 'client1',
+                    'outline' => 'outline1',
+                ],
+                [
+                    'success'    => false,
+                    'slip_entry' => [
+                        'debit'   => '3274cc74-f7ab-40a4-984a-186a593401f7',
+                        'credit'  => '471b26d0-99a1-47f4-aa57-2722f6011f2a',
+                        'client'  => 'client1',
+                        'outline' => 'outline1',
+                    ],
+                ],
             ],
         ];
     }
