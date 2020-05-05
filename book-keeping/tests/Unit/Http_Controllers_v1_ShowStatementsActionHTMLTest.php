2f51f234b9074850aa35d7f9b68acb00e571a17d
haessal <haessal@mizutamauki.net>
2020-05-05 15:29:45 +0900

    Add cases to Test ShowStatementsActionHTML

--- book-keeping/tests/Unit/Http_Controllers_v1_ShowStatementsActionHTMLTest.php
+++ book-keeping/tests/Unit/Http_Controllers_v1_ShowStatementsActionHTMLTest.php
@@ -107,6 +107,147 @@ class Http_Controllers_v1_ShowStatementsActionHTMLTest extends TestCase
     /**
      * @test
      */
+    public function __invoke_MakeContextAndReturnResponseWithEmptyBeginningDateRequest()
+    {
+        $beginning_date = '';
+        $end_date = '2019-11-30';
+        $context = [
+            'beginning_date'     => $beginning_date,
+            'end_date'           => $end_date,
+            'message'            => __('There is no item to be shown.'),
+            'display_statements' => false,
+        ];
+        $response_expected = new Response();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        $BookKeepingMock->shouldNotReceive('validatePeriod');
+        $BookKeepingMock->shouldNotReceive('retrieveStatements');
+        $BookKeepingMock->shouldNotReceive('retrieveSlips');
+        /** @var \App\Http\Responder\v1\ShowStatementsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowStatementsViewResponder::class);
+        $responderMock->shouldReceive('response')
+            ->once()
+            ->with($context)
+            ->andReturn($response_expected);
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->shouldReceive('isMethod')
+            ->once()
+            ->with('post')
+            ->andReturn(true);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('BEGINNING')
+            ->andReturn($beginning_date);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('END')
+            ->andReturn($end_date);
+
+        $controller = new ShowStatementsActionHTML($BookKeepingMock, $responderMock);
+        $response_actual = $controller->__invoke($requestMock);
+
+        $this->assertSame($response_expected, $response_actual);
+    }
+
+    /**
+     * @test
+     */
+    public function __invoke_MakeContextAndReturnResponseWithEmptyEndDateRequest()
+    {
+        $beginning_date = '2019-10-01';
+        $end_date = '';
+        $context = [
+            'beginning_date'     => $beginning_date,
+            'end_date'           => $end_date,
+            'message'            => __('There is no item to be shown.'),
+            'display_statements' => false,
+        ];
+        $response_expected = new Response();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        $BookKeepingMock->shouldNotReceive('validatePeriod');
+        $BookKeepingMock->shouldNotReceive('retrieveStatements');
+        $BookKeepingMock->shouldNotReceive('retrieveSlips');
+        /** @var \App\Http\Responder\v1\ShowStatementsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowStatementsViewResponder::class);
+        $responderMock->shouldReceive('response')
+            ->once()
+            ->with($context)
+            ->andReturn($response_expected);
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->shouldReceive('isMethod')
+            ->once()
+            ->with('post')
+            ->andReturn(true);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('BEGINNING')
+            ->andReturn($beginning_date);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('END')
+            ->andReturn($end_date);
+
+        $controller = new ShowStatementsActionHTML($BookKeepingMock, $responderMock);
+        $response_actual = $controller->__invoke($requestMock);
+
+        $this->assertSame($response_expected, $response_actual);
+    }
+
+    /**
+     * @test
+     */
+    public function __invoke_MakeContextAndReturnResponseWithInvalidDateRequest()
+    {
+        $beginning_date = '2019-09-30';
+        $end_date = '2019-09-01';
+        $context = [
+            'beginning_date'     => $beginning_date,
+            'end_date'           => $end_date,
+            'message'            => __('There is no item to be shown.'),
+            'display_statements' => false,
+        ];
+        $response_expected = new Response();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $BookKeepingMock */
+        $BookKeepingMock = Mockery::mock(BookKeepingService::class);
+        $BookKeepingMock->shouldNotReceive('validatePeriod')
+            ->once()
+            ->with($beginning_date, $end_date)
+            ->andReturn(false);
+        $BookKeepingMock->shouldNotReceive('retrieveStatements');
+        $BookKeepingMock->shouldNotReceive('retrieveSlips');
+        /** @var \App\Http\Responder\v1\ShowStatementsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowStatementsViewResponder::class);
+        $responderMock->shouldReceive('response')
+            ->once()
+            ->with($context)
+            ->andReturn($response_expected);
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+        $requestMock->shouldReceive('isMethod')
+            ->once()
+            ->with('post')
+            ->andReturn(true);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('BEGINNING')
+            ->andReturn($beginning_date);
+        $requestMock->shouldReceive('input')
+            ->once()
+            ->with('END')
+            ->andReturn($end_date);
+
+        $controller = new ShowStatementsActionHTML($BookKeepingMock, $responderMock);
+        $response_actual = $controller->__invoke($requestMock);
+
+        $this->assertSame($response_expected, $response_actual);
+    }
+
+    /**
+     * @test
+     */
     public function __invoke_MakeContextAndReturnResponseWithNoDateRequest()
     {
         $today = date('Y-m-d');
