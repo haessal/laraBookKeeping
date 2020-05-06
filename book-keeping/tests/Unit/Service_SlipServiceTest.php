de5c69c56226db5b7a990cd76983fcea8a4d9de5
haessal <haessal@mizutamauki.net>
2020-05-06 15:08:18 +0900

    Add a case to Test SlipService

--- book-keeping/tests/Unit/Service_SlipServiceTest.php
+++ book-keeping/tests/Unit/Service_SlipServiceTest.php
@@ -59,6 +59,46 @@ class Service_SlipServiceTest extends TestCase
     /**
      * @test
      */
+    public function createSlipAsDraft_CreateSlipWithSlipEntriesWhichIncludeDisplayOrder()
+    {
+        $bookId = (string) Str::uuid();
+        $outline = 'slip_outline1';
+        $date = '2019-08-17';
+        $memo = 'memo1';
+        $accountId1 = (string) Str::uuid();
+        $accountId2 = (string) Str::uuid();
+        $accountId3 = (string) Str::uuid();
+        $accountId4 = (string) Str::uuid();
+        $entries = [
+            ['debit' => $accountId1, 'credit' => $accountId2, 'amount' => 1100, 'client' => 'client11', 'outline' => 'outline11', 'display_order' => 0],
+            ['debit' => $accountId3, 'credit' => $accountId4, 'amount' => 2200, 'client' => 'client22', 'outline' => 'outline22', 'display_order' => 1],
+        ];
+        $displayOrder = 1;
+        $slipId_expected = (string) Str::uuid();
+        /** @var \App\DataProvider\SlipRepositoryInterface|\Mockery\MockInterface $slipMock */
+        $slipMock = Mockery::mock(SlipRepositoryInterface::class);
+        $slipMock->shouldReceive('create')
+            ->once()
+            ->with($bookId, $outline, $date, $memo, $displayOrder, true)
+            ->andReturn($slipId_expected);
+        /** @var \App\DataProvider\SlipEntryRepositoryInterface|\Mockery\MockInterface $slipEntryMock */
+        $slipEntryMock = Mockery::mock(SlipEntryRepositoryInterface::class);
+        $slipEntryMock->shouldReceive('create')
+            ->once()
+            ->with($slipId_expected, $entries[0]['debit'], $entries[0]['credit'], $entries[0]['amount'], $entries[0]['client'], $entries[0]['outline'], 0);
+        $slipEntryMock->shouldReceive('create')
+            ->once()
+            ->with($slipId_expected, $entries[1]['debit'], $entries[1]['credit'], $entries[1]['amount'], $entries[1]['client'], $entries[1]['outline'], 1);
+
+        $slip = new SlipService($slipMock, $slipEntryMock);
+        $slipId_actual = $slip->createSlipAsDraft($bookId, $outline, $date, $entries, $memo, $displayOrder);
+
+        $this->assertSame($slipId_expected, $slipId_actual);
+    }
+
+    /**
+     * @test
+     */
     public function createSlipEntry_CallRepositoryWithArgumentsAsItIs()
     {
         $slipId = (string) Str::uuid();
