3516003699b850e9597279effd206d5f65626244
haessal <haessal@mizutamauki.net>
2020-06-06 12:44:56 +0900

    WIP[3]: Add book list to dashbord page
    TODO: +0/-1
      [ ] logic to fetch book list (it require the function for authorization)
      [v] replace literal

--- book-keeping/resources/views/home.blade.php
+++ book-keeping/resources/views/home.blade.php
@@ -1,25 +1,37 @@
 @extends('layouts.bookkeeping')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid py-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Book List</div>
+                <div class="card-header">{{{ __('Book List') }}}</div>
                 <div class="card-body">
                     <ul class="list-group">
+                        @isset($v2_create_book_page)
                         <li class="list-group-item" style="border: 0 none;">
                             <div class="text-right">
                                 <button onclick="location.href='{{ route($v2_create_book_page) }}'" class="btn btn-success">
-                                    <i class="fa fa-book"></i>　新規作成
+                                    <i class="fa fa-book"></i>　{{{ __('New') }}}
                                 </button>
                             </div>
                         </li>
+                        @endisset
                     </ul>
                     <br>
                     <div class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp m-rky / m-rkyのブック</a>
-                        <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp kakei / kakeiのブック</a>
+                        @isset($book_list)
+                        @foreach ($book_list as $book)
+                        @isset($v2_book_page)
+                        <a href="'{{ route($v2_book_page, $book['id']) }}'" class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp {{{ $book['owner'] }}} / {{{ $book['name'] }}}</a>
+                        @else
+                        <span class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp {{{ $book['owner'] }}} / {{{ $book['name'] }}}</span>
+                        @endisset
+                        @endforeach
+                        @else
+                        <span class="text-center">{{{ __("You don't have any books yet.") }}}</span>
+                        <span class="text-center">{{{ __("Create your first book to start.") }}}</span>
+                        @endisset
                     </div>
                 </div>
             </div>
