d29e5549b2ef1982c6a7760b1d319ae998e19abe
haessal <haessal@mizutamauki.net>
2020-06-07 20:07:15 +0900

    WIP[2] Service: Book Information (Test)

--- book-keeping/tests/Unit/DataProvider_BookRepositoryInterfaceTest.php
+++ book-keeping/tests/Unit/DataProvider_BookRepositoryInterfaceTest.php
@@ -17,4 +17,20 @@ abstract class DataProvider_BookRepositoryInterfaceTest extends TestCase
 
         $this->assertTrue(is_string($bookId));
     }
+
+    /**
+     * @test
+     */
+    public function findById_ReturnValueTypeIsArrayOrNull()
+    {
+        $bookId = '3274cc74-99a1-47f4-aa57-66da432f5dad';
+
+        $book = $this->book->findById($bookId);
+
+        if (is_null($book)) {
+            $this->assertTrue(is_null($book));
+        } else {
+            $this->assertTrue(is_array($book));
+        }
+    }
 }
