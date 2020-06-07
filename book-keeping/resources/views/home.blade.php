df975b6a09be4dc9e3592a0f2e476daa325a9f75
haessal <haessal@mizutamauki.net>
2020-06-07 14:09:32 +0900

    WIP[5]: Add book list to dashbord page
    TODO: +0/-1
      [v] logic to fetch book list (it require the function for authorization)

--- book-keeping/resources/views/home.blade.php
+++ book-keeping/resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.bookkeeping')
 
+@section('pagetitle', 'Dashboard')
+
 @section('content')
 <div class="container-fluid py-4">
     <div class="row justify-content-center">
@@ -23,7 +25,7 @@
                         @isset($book_list)
                         @foreach ($book_list as $book)
                         @isset($v2_book_page)
-                        <a href="{{ route($v2_book_page, $book['id']) }}" class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp {{{ $book['owner'] }}} / {{{ $book['name'] }}}</a>
+                        <a href="{{ route($v2_book_page, ['bookId' => $book['id']]) }}" class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp {{{ $book['owner'] }}} / {{{ $book['name'] }}}</a>
                         @else
                         <span class="list-group-item list-group-item-action"><i class="fa fa-book"></i>&nbsp {{{ $book['owner'] }}} / {{{ $book['name'] }}}</span>
                         @endisset
