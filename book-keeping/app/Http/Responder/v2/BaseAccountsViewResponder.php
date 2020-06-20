7b940555fc4da11796f1572b9202188d6243a6a1
haessal <haessal@mizutamauki.net>
2020-06-20 18:21:16 +0900

    WIP[6]: Page: V2 Show & Base Accounts (responder)

---
+++ book-keeping/app/Http/Responder/v2/BaseAccountsViewResponder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Responder\v2;
+
+use App\Http\Responder\AccountsListConverter;
+
+class BaseAccountsViewResponder extends BaseViewResponder
+{
+    use AccountsListConverter;
+
+    /**
+     * List of Sub-navigation links for accounts.
+     *
+     * @return array
+     */
+    public function accountsnavilinks(): array
+    {
+        return [
+            ['name' => 'dummy_head', 'link' => null, 'caption' => ''],
+            ['name' => 'list', 'link' => 'v2_accounts', 'caption' => __('List')],
+            ['name' => 'dummy_1', 'link' => null, 'caption' => ''],
+            ['name' => 'dummy_2', 'link' => null, 'caption' => ''],
+            ['name' => 'dummy_3', 'link' => null, 'caption' => ''],
+            ['name' => 'dummy_tail', 'link' => null, 'caption' => ''],
+        ];
+    }
+}
--- book-keeping/resources/views/bookkeeping/v2/pageaccountslist.blade.php
+++ book-keeping/resources/views/bookkeeping/v2/pageaccountslist.blade.php
@@ -1,179 +1,43 @@
-@extends('bookkeeping.v2.base')
+@extends('bookkeeping.v2.baseaccounts')
 
-@section('pagetitle', 'Accounts')
-
-@section('v2_page_content')
-
-<div class="container py-4 mb-5">
-    <div class="py-3">
-        <div class="h3">資産</div>
-        <hr>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div>科目グループ1</div>
-            </div>
-            <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item bg-secondary">
-                        <div>科目11</div>
-                    </li>
-                    <li class="list-group-item bg-secondary">
-                        <div>科目12</div>
-                    </li>
-                    <li class="list-group-item">
-                        <div>科目13</div>
-                    </li>
-                </ul>
-            </div>
+@section('v2_page_accounts_content')
+<div class="row justify-content-center">
+    <div class="col-md-10">
+        <div class="py-3">
+            <div class="h3">{{{ __('Assets') }}}</div>
+            <hr>
+            @isset($accounts['asset'])
+            @foreach ($accounts['asset'] as $accountsGroupId => $accountsGroup)
+            @include('bookkeeping.v2.accountsline')
+            @endforeach
+            @endisset
         </div>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div>科目グループ2</div>
-            </div>
-            <div class="card-body">
-                <ul class="list-group">
-                    <li class="list-group-item bg-secondary">
-                        <div>科目21</div>
-                        <div>簡単な説明</div>
-                    </li>
-                    <li class="list-group-item">
-                        <div>科目22</div>
-                        <div class="text-secondary">簡単な説明</div>
-                    </li>
-                </ul>
-            </div>
+        <div class="py-3">
+            <div class="h3">{{{ __('Liabilities') }}}</div>
+            <hr>
+            @isset($accounts['liability'])
+            @foreach ($accounts['liability'] as $accountsGroupId => $accountsGroup)
+            @include('bookkeeping.v2.accountsline')
+            @endforeach
+            @endisset
         </div>
-    </div>
-    <div class="py-3">
-        <div class="h3 float-left">負債</div>
-        <div class="text-right">新規作成</div>
-        <hr>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ3</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-                <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目31</div>
-                        <div class="text-right">変更</div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="float-left">科目32</div>
-                        <div class="text-right">変更</div>
-                    </li>
-                </ul>
-            </div>
+        <div class="py-3">
+            <div class="h3">{{{ __('Expense') }}}</div>
+            <hr>
+            @isset($accounts['expense'])
+            @foreach ($accounts['expense'] as $accountsGroupId => $accountsGroup)
+            @include('bookkeeping.v2.accountsline')
+            @endforeach
+            @endisset
         </div>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ4</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-            <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目41</div>
-                        <div class="text-right">変更</div>
-                        <div class="text-left text-secondary">簡単な説明</div>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="float-left">科目42</div>
-                        <div class="text-right">変更</div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="py-3">
-        <div class="h3 float-left">費用</div>
-        <div class="text-right">新規作成</div>
-        <hr>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ5</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-                <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目51</div>
-                        <div class="text-right">変更</div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ6</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-            <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目61</div>
-                        <div class="text-right">変更</div>
-                        <div class="text-left text-secondary">簡単な説明</div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ7</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-            <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目71</div>
-                        <div class="text-right">変更</div>
-                        <div class="text-left text-secondary">簡単な説明</div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <div class="py-3">
-        <div class="h3 float-left">収益</div>
-        <div class="text-right">新規作成</div>
-        <hr>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ8</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-                <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目81</div>
-                        <div class="text-right">変更</div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="card mb-2">
-            <div class="card-header">
-                <div class="float-left">科目グループ9</div>
-                <div class="text-right">変更</div>
-            </div>
-            <div class="card-body">
-            <div class="text-right p-1">新規作成</div>
-                <ul class="list-group">
-                    <li class="list-group-item">
-                        <div class="float-left">科目91</div>
-                        <div class="text-right">変更</div>
-                        <div class="text-left text-secondary">簡単な説明</div>
-                    </li>
-                </ul>
-            </div>
+        <div class="py-3">
+            <div class="h3">{{{ __('Revenue') }}}</div>
+            <hr>
+            @isset($accounts['revenue'])
+            @foreach ($accounts['revenue'] as $accountsGroupId => $accountsGroup)
+            @include('bookkeeping.v2.accountsline')
+            @endforeach
+            @endisset
         </div>
     </div>
 </div>
