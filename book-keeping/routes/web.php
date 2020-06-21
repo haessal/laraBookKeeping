cc0305997ca44d751defcb16890180f1f04d235b
haessal <haessal@mizutamauki.net>
2020-06-21 10:51:30 +0900

    WIP[4] Page: V2 Setting Accounts

--- book-keeping/routes/web.php
+++ book-keeping/routes/web.php
@@ -38,6 +38,8 @@ Route::prefix('/page/v2/books/{bookId}')->group(function () {
     Route::get('/accounts', v2\ShowAccountsActionHtml::class)->name('v2_accounts');
     Route::get('/accounts/settings', v2\ShowAccountsSettingsActionHtml::class)->name('v2_accounts_settings');
     Route::post('/accounts/settings', v2\RedirectAccountsSettingsActionHtml::class)->name('v2_accounts_settings_redirect');
+    Route::match(['get', 'post'], '/accounts/settings/groups/{accountsGroupId}', v2\UpdateAccountsGroupActionHtml::class)->name('v2_accounts_groups');
+    Route::match(['get', 'post'], '/accounts/settings/items/{accountsItemId}', v2\UpdateAccountsItemActionHtml::class)->name('v2_accounts_items');
     Route::get('/settings', function () {
         return view('welcome');
     })->name('v2_settings');
