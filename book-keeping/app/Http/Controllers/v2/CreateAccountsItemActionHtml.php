a72870cbf80c1695cf056671298d9ba4b1421350
haessal <haessal@mizutamauki.net>
2020-06-27 08:20:05 +0900

    WIP[2] Page: V2 Add Account Item

---
+++ book-keeping/app/Http/Controllers/v2/CreateAccountsItemActionHtml.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\v2;
+
+use App\Http\Controllers\AuthenticatedBookKeepingAction;
+use App\Http\Responder\v2\CreateAccountsItemViewResponder;
+use App\Service\BookKeepingService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CreateAccountsItemActionHtml extends AuthenticatedBookKeepingAction
+{
+    /**
+     * CreateAccountsItemView responder instance.
+     *
+     * @var \App\Http\Responder\v2\CreateAccountsItemViewResponder
+     */
+    private $responder;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param \App\Service\BookKeepingService                        $BookKeeping
+     * @param \App\Http\Responder\v2\CreateAccountsItemViewResponder $responder
+     *
+     * @return void
+     */
+    public function __construct(BookKeepingService $BookKeeping, CreateAccountsItemViewResponder $responder)
+    {
+        parent::__construct($BookKeeping);
+        $this->responder = $responder;
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, string $bookId): Response
+    {
+        $context = [];
+        $context['book'] = $this->BookKeeping->retrieveBookInfomation($bookId);
+
+        return $this->responder->response($context);
+    }
+}
---
+++ book-keeping/resources/views/bookkeeping/v2/pageaccountsadditem.blade.php
@@ -0,0 +1,7 @@
+@extends('bookkeeping.v2.baseaccounts')
+
+@section('v2_page_accounts_content')
+
+Add Items
+
+@endsection
