8e4a4ec6a99c38caf84e282ddb27ce8afb0676cb
haessal <haessal@mizutamauki.net>
2020-06-27 10:57:02 +0900

    WIP[1] Page: V2 Create Account

--- book-keeping/app/Http/Responder/v2/BaseAccountsViewResponder.php
+++ book-keeping/app/Http/Responder/v2/BaseAccountsViewResponder.php
@@ -18,8 +18,7 @@ class BaseAccountsViewResponder extends BaseViewResponder
         return [
             ['name' => 'dummy_head', 'link' => null, 'caption' => ''],
             ['name' => 'list', 'link' => 'v2_accounts', 'caption' => __('List')],
-            ['name' => 'dummy_1', 'link' => null, 'caption' => ''],
-            ['name' => 'dummy_2', 'link' => null, 'caption' => ''],
+            ['name' => 'create', 'link' => 'v2_accounts_new', 'caption' => __('Add Accounts')],
             ['name' => 'settings', 'link' => 'v2_accounts_settings', 'caption' => __('Advanced Setting')],
             ['name' => 'dummy_tail', 'link' => null, 'caption' => ''],
         ];
