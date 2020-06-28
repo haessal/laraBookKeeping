6e0af285afac05898f36246a4aaeb8804249e117
haessal <haessal@mizutamauki.net>
2020-06-28 22:44:14 +0900

    Add cases to Test AccountGroupRepository

--- book-keeping/tests/Unit/DataProvider_Eloquent_AccountGroupRepositoryTest.php
+++ book-keeping/tests/Unit/DataProvider_Eloquent_AccountGroupRepositoryTest.php
@@ -57,6 +57,38 @@ class DataProvider_Eloquent_AccountGroupRepositoryTest extends DataProvider_Acco
     /**
      * @test
      */
+    public function search_ReturnedArrayHasKeysAsAccountGroup()
+    {
+        $bookId = (string) Str::uuid();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $accountGroupId = factory(AccountGroup::class)->create([
+            'book_id'               => $bookId,
+            'account_type'          => 'asset',
+            'account_group_title'   => 'dummy group title',
+            'bk_uid'                => 68,
+            'account_group_bk_code' => null,
+            'is_current'            => true,
+        ])->account_group_id;
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $accountGroupList = $this->accountGroup->search($bookId);
+
+        $this->assertFalse(count($accountGroupList) === 0);
+        if (!(count($accountGroupList) === 0)) {
+            $this->assertSame([
+                'account_group_id',
+                'account_type',
+                'account_group_title',
+                'is_current',
+                'account_group_bk_code',
+                'created_at',
+            ], array_keys($accountGroupList[0]));
+        }
+    }
+
+    /**
+     * @test
+     */
     public function update_OneRecordIsUpdated()
     {
         $bookId = (string) Str::uuid();
