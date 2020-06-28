f61fa4ce5046857b83a924370573d148edac4894
haessal <haessal@mizutamauki.net>
2020-06-29 06:47:14 +0900

    Add a case to Test AccountService

--- book-keeping/tests/Unit/Service_AccountServiceTest.php
+++ book-keeping/tests/Unit/Service_AccountServiceTest.php
@@ -100,6 +100,55 @@ class Service_AccountServiceTest extends TestCase
     /**
      * @test
      */
+    public function retrieveAccountGroups_CallRepositoryAndChangeArrayFormat()
+    {
+        $bookId = (string) Str::uuid();
+        $accountGroupId_1 = (string) Str::uuid();
+        $accountGroupId_2 = (string) Str::uuid();
+        $accountGroupId_3 = (string) Str::uuid();
+        $accountGroup_1 = [
+            'account_group_id'      => $accountGroupId_1,
+            'account_type'          => 'asset',
+            'account_group_title'   => 'group_title_1',
+            'is_current'            => true,
+            'account_group_bk_code' => 1100,
+            'created_at'            => '2020-06-01 12:00:20',
+        ];
+        $accountGroup_2 = [
+            'account_group_id'      => $accountGroupId_2,
+            'account_type'          => 'asset',
+            'account_group_title'   => 'group_title_2',
+            'is_current'            => false,
+            'account_group_bk_code' => 1200,
+            'created_at'            => '2020-06-02 12:00:20',
+        ];
+        $accountGroup_3 = [
+            'account_group_id'      => $accountGroupId_3,
+            'account_type'          => 'liability',
+            'account_group_title'   => 'group_title_3',
+            'is_current'            => false,
+            'account_group_bk_code' => 2100,
+            'created_at'            => '2020-06-02 12:00:20',
+        ];
+        $accountGroups_expected = [$accountGroupId_1 => $accountGroup_1, $accountGroupId_2 => $accountGroup_2, $accountGroupId_3 => $accountGroup_3];
+        /** @var \App\DataProvider\AccountGroupRepositoryInterface|\Mockery\MockInterface $accountGroupMock */
+        $accountGroupMock = Mockery::mock(AccountGroupRepositoryInterface::class);
+        $accountGroupMock->shouldReceive('search')
+            ->once()
+            ->with($bookId)
+            ->andReturn([$accountGroup_1, $accountGroup_2, $accountGroup_3]);
+        /** @var \App\DataProvider\AccountRepositoryInterface|\Mockery\MockInterface $accountMock */
+        $accountMock = Mockery::mock(AccountRepositoryInterface::class);
+
+        $account = new AccountService($accountMock, $accountGroupMock);
+        $accountGroups_actual = $account->retrieveAccountGroups($bookId);
+
+        $this->assertSame($accountGroups_expected, $accountGroups_actual);
+    }
+
+    /**
+     * @test
+     */
     public function updateAccount_CallRepositoryWithArgumentsAsItIs()
     {
         $accountId = (string) Str::uuid();
