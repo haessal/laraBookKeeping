4e8c8e40695f63d3a0708b593264a82cc24132ce
haessal <haessal@mizutamauki.net>
2020-07-04 18:42:26 +0900

    WIP[1] Support API: GET /v1/slipentries

---
+++ book-keeping/app/Http/Controllers/api/v1/GetSlipEntriesActionApi.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\api\AuthenticatedBookKeepingActionApi;
+use App\Http\Responder\api\v1\SlipEntriesJsonResponder;
+use App\Service\BookKeepingService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class GetSlipEntriesActionApi extends AuthenticatedBookKeepingActionApi
+{
+    /**
+     * SlipEntriesJson responder instance.
+     *
+     * @var \App\Http\Responder\api\v1\SlipEntriesJsonResponder
+     */
+    private $responder;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param \App\Service\BookKeepingService                     $BookKeeping
+     * @param \App\Http\Responder\api\v1\SlipEntriesJsonResponder $responder
+     *
+     * @return void
+     */
+    public function __construct(BookKeepingService $BookKeeping, SlipEntriesJsonResponder $responder)
+    {
+        parent::__construct($BookKeeping);
+        $this->responder = $responder;
+    }
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $result = $this->validateAndTrimSlipEntriesQuery($request->all());
+        if ($result['success']) {
+            $query = $result['query'];
+            $context['slips'] = $this->BookKeeping->retrieveSlips($query['from'], $query['to'], $query['debit'], $query['credit'], $query['operand'], $query['keyword']);
+            $response = $this->responder->response($context);
+        } else {
+            $response = new JsonResponse(null, JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Validate query.
+     *
+     * @param array $queries
+     *
+     * @return array
+     */
+    public function validateAndTrimSlipEntriesQuery($queries): array
+    {
+        $success = true;
+        $from = null;
+        $to = null;
+        $debit = null;
+        $credit = null;
+        $operand = null;
+        $keyword = null;
+
+        foreach ($queries as $queryKey => $query) {
+            switch ($queryKey) {
+                case 'from':
+                    $from = trim($query);
+                    break;
+                case 'to':
+                    $to = trim($query);
+                    break;
+                case 'debit':
+                    $debit = trim($query);
+                    break;
+                case 'credit':
+                    $credit = trim($query);
+                    break;
+                case 'operand':
+                    $operand = trim($query);
+                    break;
+                case 'keyword':
+                    $keyword = trim($query);
+                    break;
+                default:
+                    $success = false;
+                    break;
+            }
+        }
+        if (empty($from) && empty($to) && empty($debit) && empty($credit) && empty($keyword) ) {
+            $success = false;
+        }
+        if (!($this->BookKeeping->validatePeriod($from, $to))) {
+            $success = false;
+        }
+        if (!empty($operand) && ($operand != 'and') && ($operand != 'or')) {
+            $success = false;
+        }
+        if (!empty($debit) && !($this->validateUuid($debit))) {
+            $success = false;
+        }
+        if (!empty($credit) && !($this->validateUuid($credit))) {
+            $success = false;
+        }
+        if (!empty($debit) && !empty($credit) && empty($operand)) {
+            $success = false;
+        }
+        $trimmed_query = ['from' => $from, 'to' => $to, 'debit' => $debit, 'credit' => $credit, 'operand' => $operand, 'keyword' => $keyword];
+
+        return ['success' => $success, 'query' => $trimmed_query];
+    }
+
+    /**
+     * Check if UUID is in valid format.
+     *
+     * @param array $uuid
+     *
+     * @return bool
+     */
+    private function validateUuid(string $uuid): bool
+    {
+        return (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) === 1);
+    }
+}
