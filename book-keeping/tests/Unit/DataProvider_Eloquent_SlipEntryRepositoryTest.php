e8445453f973d5f95e054283d77f410d714c03ae
haessal <haessal@mizutamauki.net>
2020-07-12 14:40:51 +0900

    WIP[6] Support API: PATCH /v1/slipentries/{slipEntryId} (Test)

--- book-keeping/tests/Unit/DataProvider_Eloquent_SlipEntryRepositoryTest.php
+++ book-keeping/tests/Unit/DataProvider_Eloquent_SlipEntryRepositoryTest.php
@@ -824,4 +824,52 @@ class DataProvider_Eloquent_SlipEntryRepositoryTest extends DataProvider_SlipEnt
             ], array_keys($slipEntries[0]));
         }
     }
+
+    /**
+     * @test
+     */
+    public function update_OneRecordIsUpdated()
+    {
+        $slipId = (string) Str::uuid();
+        $debit = (string) Str::uuid();
+        $credit = (string) Str::uuid();
+        $amount = 836;
+        $client = 'client837';
+        $outline = 'outlin838';
+        $debit_updated = (string) Str::uuid();
+        $credit_updated = (string) Str::uuid();
+        $amount_updated = 841;
+        $client_updated = 'client_updated842';
+        $outline_updated = 'outlin_updated843';
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $slipEntryId = factory(SlipEntry::class)->create([
+            'slip_id' => $slipId,
+            'debit'   => $debit,
+            'credit'  => $credit,
+            'amount'  => $amount,
+            'client'  => $client,
+            'outline' => $outline,
+        ])->slip_entry_id;
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $this->slipEntry->update($slipEntryId, [
+            'debit'   => $debit_updated,
+            'credit'  => $credit_updated,
+            'amount'  => $amount_updated,
+            'client'  => $client_updated,
+            'outline' => $outline_updated,
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->assertDatabaseHas('bk2_0_slip_entries', [
+            'slip_entry_id' => $slipEntryId,
+            'slip_id'       => $slipId,
+            'debit'         => $debit_updated,
+            'credit'        => $credit_updated,
+            'amount'        => $amount_updated,
+            'client'        => $client_updated,
+            'outline'       => $outline_updated,
+        ]);
+    }
 }
