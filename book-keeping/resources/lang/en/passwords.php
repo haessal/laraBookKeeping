893417e5e3103bee4da6f9a1e21b3d50a64254b8
haessal <haessal@mizutamauki.net>
2021-05-06 10:44:34 +0900

    Modify 'waiting before retrying password reset'
    'waiting before retrying password reset' was added in Laravel 6. The function
    expects column 'email' and doesn't work as is. So, fixed to be able to treat
    also 'name' as index. And some settings related this function is added.

--- book-keeping/resources/lang/en/passwords.php
+++ book-keeping/resources/lang/en/passwords.php
@@ -13,11 +13,12 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'reset'    => 'Your password has been reset!',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that name.",
-    'link'     => 'It looks like you clicked on an invalid password reset link. Please try again.',
+    'password'  => 'Passwords must be at least eight characters and match the confirmation.',
+    'reset'     => 'Your password has been reset!',
+    'sent'      => 'We have e-mailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token'     => 'This password reset token is invalid.',
+    'user'      => "We can't find a user with that name.",
+    'link'      => 'It looks like you clicked on an invalid password reset link. Please try again.',
 
 ];
