4d0067d7ebad274df5b9eccf344278ffca9c0087
haessal <haessal@mizutamauki.net>
2021-05-09 12:41:59 +0900

    Test ShowDashboardViewResponder

---
+++ book-keeping/tests/Unit/Http_Responder_ShowDashboardViewResponderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Responder\ShowDashboardViewResponder;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Response;
+use Mockery;
+use Tests\TestCase;
+
+class Http_Responder_ShowDashboardViewResponderTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @test
+     */
+    public function response_ReturnResponse()
+    {
+        $context = [];
+        /** @var \Illuminate\Http\Response|\Mockery\MockInterface $ResponseMock */
+        $ResponseMock = Mockery::mock(Response::class);
+        $ResponseMock->shouldReceive('setContent')->once();
+        $ResponseMock->shouldReceive('setStatusCode')->once();
+        /** @var \Illuminate\Contracts\View\Factory|\Mockery\MockInterface $ViewFactoryMock */
+        $ViewFactoryMock = Mockery::mock(Factory::class);
+        $ViewFactoryMock->shouldReceive('make')->once();
+
+        $responder = new ShowDashboardViewResponder($ResponseMock, $ViewFactoryMock);
+        $response = $responder->response($context);
+
+        $this->assertTrue(true);
+    }
+}
