82e7c76590e128ea219f692e5d71e4973f0a7585
haessal <haessal@mizutamauki.net>
2021-05-09 12:43:45 +0900

    Apply the base template to Dashboard page

--- book-keeping/resources/views/home.blade.php
+++ book-keeping/resources/views/home.blade.php
@@ -1,7 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.bookkeeping')
+
+@section('pagetitle', 'Dashboard')
 
 @section('content')
-<div class="container">
+<div class="container-fluid py-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
--- book-keeping/routes/web.php
+++ book-keeping/routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', ShowDashboardActionHtml::class)->name('home');
 
 Route::get('/settings', function () {
     return redirect()->route('settings_tokens');
