77673507621f645ef655b9a7fb81cd1782cd17a0
haessal <haessal@mizutamauki.net>
2021-07-09 17:08:18 +0900

    Add a responder to provide navigation items for all Book related pages

---
+++ book-keeping/app/Http/Responder/v2/BaseViewResponder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Responder\v2;
+
+use App\Http\Responder\BaseLayoutViewResponder;
+use Illuminate\Contracts\View\Factory as ViewFactory;
+use Illuminate\Http\Response;
+
+class BaseViewResponder extends BaseLayoutViewResponder
+{
+    /**
+     * Response instance.
+     *
+     * @var \Illuminate\Http\Response
+     */
+    protected $response;
+
+    /**
+     * View Factory instance.
+     *
+     * @var \Illuminate\Contracts\View\Factory
+     */
+    protected $view;
+
+    /**
+     * Create a new BaseViewResponder instance.
+     *
+     * @param \Illuminate\Http\Response          $response
+     * @param \Illuminate\Contracts\View\Factory $view
+     */
+    public function __construct(Response $response, ViewFactory $view)
+    {
+        $this->response = $response;
+        $this->view = $view;
+    }
+
+    /**
+     * List of Navigation links for version 2 pages.
+     *
+     * @return array
+     */
+    public function navilinks(): array
+    {
+        return [
+            ['link' => null],
+            ['link' => null],
+            ['link' => null],
+            ['link' => null],
+            ['link' => null],
+            ['link' => null],
+        ];
+    }
+}
