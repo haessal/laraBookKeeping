aa01982bdf943ef81ff3ac75706c340a22298410
haessal <haessal@mizutamauki.net>
2022-07-16 19:22:14 +0900

    Change DB column name for foreign key to be simple

--- book-keeping/database/migrations/2019_09_09_174358_create_account_groups_table.php
+++ book-keeping/database/migrations/2019_09_09_174358_create_account_groups_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('bk2_0_account_groups', function (Blueprint $table) {
             $table->uuid('account_group_id')->primary();
-            $table->uuid('book_bound_on');
-            $table->foreign('book_bound_on')->references('book_id')->on('bk2_0_books');
+            $table->uuid('book_id');
+            $table->foreign('book_id')->references('book_id')->on('bk2_0_books');
             $table->enum('account_type', ['asset', 'liability', 'expense', 'revenue']);
             $table->string('account_group_title', 40);
             $table->unsignedBigInteger('bk_uid')->nullable();
--- book-keeping/database/migrations/2019_09_09_174722_create_slip_group_entries_table.php
+++ book-keeping/database/migrations/2019_09_09_174722_create_slip_group_entries_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('bk2_0_slip_group_entries', function (Blueprint $table) {
             $table->uuid('slip_group_entry_id')->primary();
-            $table->uuid('slip_group_bound_on');
-            $table->foreign('slip_group_bound_on')->references('slip_group_id')->on('bk2_0_slip_groups');
+            $table->uuid('slip_group_id');
+            $table->foreign('slip_group_id')->references('slip_group_id')->on('bk2_0_slip_groups');
             $table->uuid('related_slip');
             $table->foreign('related_slip')->references('slip_id')->on('bk2_0_slips');
             $table->timestamps();
