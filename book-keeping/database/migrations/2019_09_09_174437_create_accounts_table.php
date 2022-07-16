1a1ef607d7eb4ca40b5093d02d878ac429a00831
haessal <haessal@mizutamauki.net>
2022-07-16 19:28:36 +0900

    Change DB column name to tell two 'bk_code's apart

--- book-keeping/database/migrations/2019_09_09_174437_create_accounts_table.php
+++ book-keeping/database/migrations/2019_09_09_174437_create_accounts_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('description', 200);
             $table->boolean('selectable');
             $table->unsignedBigInteger('bk_uid')->nullable();
-            $table->unsignedBigInteger('bk_code')->nullable();
+            $table->unsignedBigInteger('account_bk_code')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
