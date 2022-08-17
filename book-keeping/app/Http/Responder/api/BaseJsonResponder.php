46ed27b7ca26369f3786208bc0533b7fbe3ab8c4
haessal <haessal@mizutamauki.net>
2022-08-17 12:13:16 +0900

    WIP: API

---
+++ book-keeping/app/Http/Responder/api/BaseJsonResponder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Responder\api;
+
+use Illuminate\Http\JsonResponse;
+
+class BaseJsonResponder
+{
+    /**
+     * JsonResponse instance.
+     *
+     * @var \Illuminate\Http\JsonResponse
+     */
+    protected $response;
+
+    /**
+     * Create a new BaseJsonResponder instance.
+     *
+     * @param  \Illuminate\Http\JsonResponse  $response
+     */
+    public function __construct(JsonResponse $response)
+    {
+        $this->response = $response;
+    }
+}
