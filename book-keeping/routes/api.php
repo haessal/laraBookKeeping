89edfb886c1c79daa195f5f93dbd704089d75376
haessal <haessal@mizutamauki.net>
2022-09-04 01:40:00 +0900

    WIP: route

--- book-keeping/routes/api.php
+++ book-keeping/routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\api\v1\DeleteBooksPermissions;
 use App\Http\Controllers\api\v1\DeleteSlipEntriesActionApi;
 use App\Http\Controllers\api\v1\GetAccountsActionApi;
+use App\Http\Controllers\api\v1\GetBooksActionApi;
+use App\Http\Controllers\api\v1\GetBooksBookIdActionApi;
+use App\Http\Controllers\api\v1\GetBooksPermissions;
 use App\Http\Controllers\api\v1\GetSlipEntriesActionApi;
 use App\Http\Controllers\api\v1\GetSlipEntriesSlipEntryIdActionApi;
 use App\Http\Controllers\api\v1\GetSlipsSlipIdActionApi;
+use App\Http\Controllers\api\v1\PatchBooksActionApi;
 use App\Http\Controllers\api\v1\PatchSlipEntriesActionApi;
 use App\Http\Controllers\api\v1\PatchSlipsActionApi;
+use App\Http\Controllers\api\v1\PostBooksActionApi;
+use App\Http\Controllers\api\v1\PostBooksPermissions;
 use App\Http\Controllers\api\v1\PostSlipsActionApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +34,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
+    Route::get('/books', GetBooksActionApi::class);
+    Route::post('/books', PostBooksActionApi::class);
+    Route::get('/books{bookId}', GetBooksBookIdActionApi::class);
+    Route::patch('/books/{bookId}', PatchBooksActionApi::class);
+
+    Route::get('/books/{bookId}/permissions', GetBooksPermissions::class);
+    Route::post('/books/{bookId}/permissions', PostBooksPermissions::class);
+    Route::delete('/books/{bookId}/permissions', DeleteBooksPermissions::class);
+
     Route::get('/accounts', GetAccountsActionApi::class);
 
     Route::post('/slips', PostSlipsActionApi::class);
