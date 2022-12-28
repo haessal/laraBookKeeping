e63f117659c2a9f751365ba1aeefe517692ed384
haessal <haessal@mizutamauki.net>
2022-12-28 17:06:55 +0900

    Implement views for basic layout

--- book-keeping/resources/views/layouts/app.blade.php
+++ book-keeping/resources/views/layouts/app.blade.php
@@ -1,35 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        @if (isset($title))
+        <title>{{ $title }} - {{ config('app.name', 'BookKeeping') }}</title>
+        @else
+        <title>{{ config('app.name', 'BookKeeping') }}</title>
+        @endif
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
+    <body class="bg-white">
+        <div class="min-h-screen">
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <header class="bg-gray-50 p-4">
+                <div>@include('layouts.navigation')</div>
+                @if (isset($header))
+                <div>{{ $header }}</div>
+                @endif
+            </header>
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <main>{{ $slot }}</main>
+
+            <!-- Page Footer -->
+            <footer>
+                <div class="flex justify-end bg-gray-900 py-1 text-gray-100">
+                    <address class="px-4">&copy 2007 haessal</address>
+                </div>
+            </footer>
         </div>
     </body>
 </html>
--- book-keeping/resources/views/settings/accesstokens.blade.php
+++ book-keeping/resources/views/settings/accesstokens.blade.php
@@ -1,47 +1,31 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Personal access tokens') }}
-        </h2>
-    </x-slot>
+    <x-slot name="title">{{ __('Personal access tokens') }}</x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+        <div class="mx-auto max-w-7xl px-6 md:px-8">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">{{ __('Personal access tokens') }}</h2>
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="border-b border-gray-200 bg-white p-6">
                     <form method="POST" action="{{ route('settings_tokens') }}">
                         @csrf
-                        <x-primary-button type="submit">
-                            {{ __('Generate new token') }}
-                        </x-primary-button>
+                        <x-primary-button type="submit">{{ __('Generate new token') }}</x-primary-button>
                     </form>
-                    <br>
+                    <br />
                     <form method="POST" action="{{ route('settings_tokens') }}">
                         @csrf
-                        <input name="_method" type="hidden" value="DELETE">
-                        <x-primary-button type="submit">
-                            {{ __('Delete token') }}
-                        </x-primary-button>
+                        <input name="_method" type="hidden" value="DELETE" />
+                        <x-primary-button type="submit">{{ __('Delete token') }}</x-primary-button>
                     </form>
-                    <br>
-                    @if ($token != null)
-                        @if ($message_for_new_token != null)
-                            {{ $message_for_new_token }}
-                            <br>
-                        @endif
-                        {{ $token }}
-                    @else
-                        @if ($message_for_no_token != null)
-                            {{ $message_for_no_token }}
-                            <br>
-                        @else
-                            {{ $timestamp }}
-                            <br>
-                        @endif
-                    @endif
+                    <br />
+                    @if ($token != null) @if ($message_for_new_token != null) {{ $message_for_new_token }}
+                    <br />
+                    @endif {{ $token }} @else @if ($message_for_no_token != null) {{ $message_for_no_token }}
+                    <br />
+                    @else {{ $timestamp }}
+                    <br />
+                    @endif @endif
                 </div>
             </div>
         </div>
     </div>
-
 </x-app-layout>
