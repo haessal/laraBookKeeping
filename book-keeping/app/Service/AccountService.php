b9317e7e8dc463c1c3410c1c78ef40eec47bbe13
haessal <haessal@mizutamauki.net>
2023-01-29 15:33:08 +0900

    Services

---
+++ book-keeping/app/Service/AccountService.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Service;
+
+use App\DataProvider\AccountGroupRepositoryInterface;
+use App\DataProvider\AccountRepositoryInterface;
+
+class AccountService
+{
+    /**
+     * The key of Asset in the "account_type" column.
+     *
+     * @var string
+     */
+    const ACCOUNT_TYPE_ASSET = 'asset';
+
+    /**
+     * The key of Liability in the "account_type" column.
+     *
+     * @var string
+     */
+    const ACCOUNT_TYPE_LIABILITY = 'liability';
+
+    /**
+     * The key of Expense in the "account_type" column.
+     *
+     * @var string
+     */
+    const ACCOUNT_TYPE_EXPENSE = 'expense';
+
+    /**
+     * The key of Revenue in the "account_type" column.
+     *
+     * @var string
+     */
+    const ACCOUNT_TYPE_REVENUE = 'revenue';
+
+    /**
+     * Account repository instance.
+     *
+     * @var \App\DataProvider\AccountRepositoryInterface
+     */
+    private $account;
+
+    /**
+     * Account group repository instance.
+     *
+     * @var \App\DataProvider\AccountGroupRepositoryInterface
+     */
+    private $accountGroup;
+
+    /**
+     * Create a new AccountService instance.
+     *
+     * @param  \App\DataProvider\AccountRepositoryInterface  $account
+     * @param  \App\DataProvider\AccountGroupRepositoryInterface  $accountGroup
+     */
+    public function __construct(AccountRepositoryInterface $account, AccountGroupRepositoryInterface $accountGroup)
+    {
+        $this->account = $account;
+        $this->accountGroup = $accountGroup;
+    }
+
+    /**
+     * Create new Account.
+     *
+     * @param  string  $accountGroupId
+     * @param  string  $title
+     * @param  string  $description
+     * @param  int  $bk_uid
+     * @param  int  $bk_code
+     * @return string $accountId
+     */
+    public function createAccount(string $accountGroupId, string $title, string $description, int $bk_uid = null, int $bk_code = null): string
+    {
+        $accountId = $this->account->create($accountGroupId, $title, $description, $bk_uid, $bk_code);
+
+        return $accountId;
+    }
+
+    /**
+     * Create new Account Group.
+     *
+     * @param  string  $bookId
+     * @param  string  $accountType
+     * @param  string  $title
+     * @param  bool  $isCurrent
+     * @param  int  $bk_uid
+     * @param  int  $bk_code
+     * @return string $accountGroupId
+     */
+    public function createAccountGroup(string $bookId, string $accountType, string $title, bool $isCurrent = false, int $bk_uid = null, int $bk_code = null): string
+    {
+        $accountGroupId = $this->accountGroup->create($bookId, $accountType, $title, $isCurrent, $bk_uid, $bk_code);
+
+        return $accountGroupId;
+    }
+
+    /**
+     * Retrieve list of account.
+     *
+     * @param  string  $bookId
+     * @return array
+     */
+    public function retrieveAccounts(string $bookId): array
+    {
+        $accounts = [];
+        $accountList = $this->account->searchBook($bookId);
+
+        foreach ($accountList as $accountItem) {
+            $accounts[$accountItem['account_id']] = $accountItem;
+        }
+
+        return $accounts;
+    }
+
+    /**
+     * Retrieve list of account group.
+     *
+     * @param  string  $bookId
+     * @return array
+     */
+    public function retrieveAccountGroups(string $bookId): array
+    {
+        $accountGroups = [];
+        $accountGroupList = $this->accountGroup->searchBook($bookId);
+
+        foreach ($accountGroupList as $accountGroup) {
+            $accountGroups[$accountGroup['account_group_id']] = $accountGroup;
+        }
+
+        return $accountGroups;
+    }
+
+    /**
+     * Update Account.
+     *
+     * @param  string  $accountId
+     * @param  array  $newData
+     */
+    public function updateAccount(string $accountId, array $newData)
+    {
+        $this->account->update($accountId, $newData);
+    }
+
+    /**
+     * Update Account Group.
+     *
+     * @param  string  $accountGroupId
+     * @param  array  $newData
+     */
+    public function updateAccountGroup(string $accountGroupId, array $newData)
+    {
+        $this->accountGroup->update($accountGroupId, $newData);
+    }
+}
---
+++ book-keeping/app/Service/BookService.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Service;
+
+use App\DataProvider\BookRepositoryInterface;
+use App\DataProvider\PermissionRepositoryInterface;
+
+class BookService
+{
+    /**
+     * Book repository instance.
+     *
+     * @var \App\DataProvider\BookRepositoryInterface
+     */
+    private $book;
+
+    /**
+     * Permission repository instance.
+     *
+     * @var \App\DataProvider\PermissionRepositoryInterface
+     */
+    private $permission;
+
+    /**
+     * Create a new BookService instance.
+     *
+     * @param  \App\DataProvider\BookRepositoryInterface  $book
+     * @param  \App\DataProvider\PermissionRepositoryInterface  $app
+     */
+    public function __construct(BookRepositoryInterface $book, PermissionRepositoryInterface $permission)
+    {
+        $this->book = $book;
+        $this->permission = $permission;
+    }
+
+    /**
+     * Create new Book.
+     *
+     * @param  int  $userId
+     * @param  string  $title
+     * @return string $bookId
+     */
+    public function createBook(int $userId, string $title): string
+    {
+        $bookId = $this->book->create($title);
+        $permissionId = $this->permission->create($userId, $bookId);
+
+        return $bookId;
+    }
+
+    /**
+     * Owner of the specified Book.
+     *
+     * @param  int  $bookId
+     * @return string | null
+     */
+    public function ownerName(string $bookId): ?string
+    {
+        $ownerName = null;
+        $user = $this->permission->findOwnerOfBook($bookId);
+        if (! empty($user)) {
+            $ownerName = $user['name'];
+        }
+
+        return $ownerName;
+    }
+
+    /**
+     * Retrieve list of accessable Book.
+     *
+     * @param  int  $userId
+     * @return array
+     */
+    public function retrieveBookList(int $userId): array
+    {
+        $booklist = $this->permission->findAccessibleBooks($userId);
+
+        return $booklist;
+    }
+
+    /**
+     * Retrieve default Book.
+     *
+     * @param  int  $userId
+     * @return string | null
+     */
+    public function retrieveDefaultBook(int $userId)
+    {
+        $bookId = $this->permission->findDefaultBook($userId);
+
+        return $bookId;
+    }
+
+    /**
+     * Retrieve information.
+     *
+     * @param  string  $bookId
+     * @return array | null
+     */
+    public function retrieveInformation(string $bookId): ?array
+    {
+        $book = $this->book->findById($bookId);
+
+        return $book;
+    }
+}
