f8c2c3b7252c5a6baf80457d46d4b99021a93863
haessal <haessal@mizutamauki.net>
2023-01-29 15:33:08 +0900

    Services

---
+++ book-keeping/app/Service/BookService.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace App\Service;
+
+use App\DataProvider\BookRepositoryInterface;
+use App\DataProvider\PermissionRepositoryInterface;
+
+class BookService
+{
+    /**
+     * Book repository instance.
+     *
+     * @var \App\DataProvider\BookRepositoryInterface
+     */
+    private $book;
+
+    /**
+     * Permission repository instance.
+     *
+     * @var \App\DataProvider\PermissionRepositoryInterface
+     */
+    private $permission;
+
+    /**
+     * Create a new BookService instance.
+     *
+     * @param  \App\DataProvider\BookRepositoryInterface  $book
+     * @param  \App\DataProvider\PermissionRepositoryInterface  $permission
+     */
+    public function __construct(BookRepositoryInterface $book, PermissionRepositoryInterface $permission)
+    {
+        $this->book = $book;
+        $this->permission = $permission;
+    }
+
+    /**
+     * Create a new book.
+     *
+     * @param  int  $userId
+     * @param  string  $title
+     * @return string
+     */
+    public function createBook($userId, $title)
+    {
+        $bookId = $this->book->create($title);
+        $this->permission->create($userId, $bookId, true, true, false);
+
+        return $bookId;
+    }
+
+    /**
+     * Create a new permission.
+     *
+     * @param  string  $bookId
+     * @param  string  $userName
+     * @param  'ReadWrite'|'ReadOnly'  $mode
+     * @return array{user: string, permitted_to: 'ReadWrite'|'ReadOnly'}|null
+     */
+    public function createPermission($bookId, $userName, $mode): ?array
+    {
+        $permission = null;
+        $user = $this->permission->findUserByName($userName);
+        if (isset($user)) {
+            $modifiable = ($mode == 'ReadWrite') ? true : false;
+            $this->permission->create(intval($user['id']), $bookId, $modifiable, false, false);
+            $permission = ['user' => $userName, 'permitted_to' => $mode];
+        }
+
+        return $permission;
+    }
+
+    /**
+     * Delete the permission that the user access to the book.
+     *
+     * @param  string  $bookId
+     * @param  string  $userName
+     * @return void
+     */
+    public function deletePermission($bookId, $userName)
+    {
+        $user = $this->permission->findUserByName($userName);
+        if (isset($user)) {
+            $this->permission->delete(intval($user['id']), $bookId);
+        }
+    }
+
+    /**
+     * Retrieve the book if it is available to the user.
+     *
+     * @param  string  $bookId
+     * @param  int  $userId
+     * @return array{
+     *   book_id: string,
+     *   book_name: string,
+     *   modifiable: bool,
+     *   is_owner: bool,
+     *   is_default: bool,
+     *   created_at: string,
+     * }|null
+     */
+    public function retrieveBook($bookId, $userId): ?array
+    {
+        $book = $this->permission->findBook($userId, $bookId);
+
+        return is_null($book) ? null : [
+            'book_id'    => strval($book['book_id']),
+            'book_name'  => strval($book['book_name']),
+            'modifiable' => boolval($book['modifiable']),
+            'is_owner'   => boolval($book['is_owner']),
+            'is_default' => boolval($book['is_default']),
+            'created_at' => strval($book['created_at']),
+        ];
+    }
+
+    /**
+     * Retrieve a list of books that is available to the user.
+     *
+     * @param  int  $userId
+     * @return array{
+     *   book_id: string,
+     *   book_name: string,
+     *   modifiable: bool,
+     *   is_owner: bool,
+     *   is_default: bool,
+     *   created_at: string,
+     * }[]
+     */
+    public function retrieveBooks($userId): array
+    {
+        $booklist = [];
+
+        $list = $this->permission->searchForAccessibleBooks($userId);
+        foreach ($list as $book) {
+            $booklist[] = [
+                'book_id'    => strval($book['book_id']),
+                'book_name'  => strval($book['book_name']),
+                'modifiable' => boolval($book['modifiable']),
+                'is_owner'   => boolval($book['is_owner']),
+                'is_default' => boolval($book['is_default']),
+                'created_at' => strval($book['created_at']),
+            ];
+        }
+
+        return $booklist;
+    }
+
+    /**
+     * Retrieve the default book of the user.
+     *
+     * @param  int  $userId
+     * @return string|null
+     */
+    public function retrieveDefaultBook($userId)
+    {
+        $bookId = $this->permission->findDefaultBook($userId);
+
+        return $bookId;
+    }
+
+    /**
+     * Retrieve the information of the book.
+     *
+     * @param  string  $bookId
+     * @return array{
+     *   book_id: string,
+     *   book_name: string,
+     * }|null
+     */
+    public function retrieveInformationOf($bookId): ?array
+    {
+        $book = $this->book->findById($bookId);
+
+        return is_null($book) ? null : [
+            'book_id'   => strval($book['book_id']),
+            'book_name' => strval($book['book_name']),
+        ];
+    }
+
+    /**
+     * Retrieve the name of the owner of the book.
+     *
+     * @param  string  $bookId
+     * @return string|null
+     */
+    public function retrieveOwnerNameOf($bookId)
+    {
+        $user = $this->permission->findOwnerOfBook($bookId);
+
+        return is_null($user) ? null : $user['name'];
+    }
+
+    /**
+     * Retrieve a list of permissions related to the book.
+     *
+     * @param  string  $bookId
+     * @return array{user: string, permitted_to: 'ReadWrite'|'ReadOnly'}[]
+     */
+    public function retrievePermissions($bookId): array
+    {
+        $permissions = [];
+        $permission_list = $this->permission->findByBookId($bookId);
+        foreach ($permission_list as $item) {
+            $user = $this->permission->findUser(intval($item['permitted_user']));
+            if (isset($user)) {
+                $permissions[] = [
+                    'user' => strval($user['name']),
+                    'permitted_to' => ($item['modifiable'] != 0) ? 'ReadWrite' : 'ReadOnly',
+                ];
+            }
+        }
+
+        return $permissions;
+    }
+
+    /**
+     * Update the mark for indicating that the book is default one for the user.
+     *
+     * @param  string  $bookId
+     * @param  int  $userId
+     * @param  bool  $isDefault
+     * @return void
+     */
+    public function updateDefaultMarkOf($bookId, $userId, $isDefault)
+    {
+        $this->permission->updateDefaultBookMark($userId, $bookId, $isDefault);
+    }
+
+    /**
+     * Update the name of the book.
+     *
+     * @param  string  $bookId
+     * @param  string  $newName
+     * @return void
+     */
+    public function updateNameOf($bookId, $newName)
+    {
+        $this->book->updateName($bookId, $newName);
+    }
+}
