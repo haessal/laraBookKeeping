cfc20ba40be8a74beac723e6d1f0edb287335557
haessal <haessal@mizutamauki.net>
2023-02-23 23:18:21 +0900

    Specify the user who want to reset password by name

--- book-keeping/app/Models/User.php
+++ book-keeping/app/Models/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Lang;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
@@ -41,4 +44,24 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        ResetPasswordNotification::toMailUsing(function ($notifiable, $token) {
+            return (new MailMessage())
+                ->subject(Lang::get('Reset Password Notification'))
+                ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+                ->action(Lang::get('Reset Password'), url(config('app.url').route('password.reset', ['token' => $token, 'name' => $notifiable->name], false)))
+                ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+                ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+        });
+
+        $this->notify((new ResetPasswordNotification($token)));
+    }
 }
