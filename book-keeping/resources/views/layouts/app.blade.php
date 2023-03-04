def8cf34304bbb6bae8dbd23e8420c44ef288695
haessal <haessal@mizutamauki.net>
2023-03-04 19:31:23 +0900

    Implement views for basic layout

--- book-keeping/resources/views/layouts/app.blade.php
+++ book-keeping/resources/views/layouts/app.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        @if (isset($title))
+        <title>{{ $title }} - {{ config('app.name', 'BookKeeping') }}</title>
+        @else
+        <title>{{ config('app.name', 'BookKeeping') }}</title>
+        @endif
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
@@ -14,19 +18,26 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
 
+    <body class="bg-white dark:bg-black">
+        <div class="min-h-screen">
             <!-- Page Heading -->
-            @if (isset($header))
-            <header class="bg-white shadow dark:bg-gray-800">
-                <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">{{ $header }}</div>
+            <header class="bg-gray-50 p-4 dark:bg-gray-900">
+                <div>@include('layouts.navigation')</div>
+                @if (isset($header))
+                <div>{{ $header }}</div>
+                @endif
             </header>
-            @endif
 
             <!-- Page Content -->
             <main>{{ $slot }}</main>
+
+            <!-- Page Footer -->
+            <footer>
+                <div class="flex justify-end bg-gray-900 py-1 text-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <address class="px-4">&copy 2007 haessal</address>
+                </div>
+            </footer>
         </div>
     </body>
 </html>
