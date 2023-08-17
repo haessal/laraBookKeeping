f06b175f19cdaa18bf426048e1c57e27d79ecd8e
haessal <haessal@mizutamauki.net>
2023-08-17 19:40:38 +0900

    WIP: Add Test for v1 view templary

---
+++ book-keeping/tests/Feature/page/v1/Top/TopPageDisplayTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\page\v1\Top;
+
+use App\Models\Book;
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TopPageDisplayTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @var \App\Models\User */
+    private $user;
+
+    /** @var \App\Models\User */
+    private $otherUser;
+
+    /** @var \App\Models\Book */
+    private $book;
+
+    /** @var \App\Models\Book */
+    private $sharedBook;
+
+    /** @var \App\Models\Book */
+    private $unavailableBook;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->otherUser = User::factory()->create();
+        $this->book = Book::factory()->create([
+            'book_name' => $this->faker->word(),
+        ]);
+        Permission::factory()->create([
+            'permitted_user' => $this->user->id,
+            'readable_book'  => $this->book->book_id,
+            'modifiable'     => true,
+            'is_owner'       => true,
+            'is_default'     => true,
+        ]);
+        $this->sharedBook = Book::factory()->create([
+            'book_name' => $this->faker->word(),
+        ]);
+        Permission::factory()->create([
+            'permitted_user' => $this->otherUser->id,
+            'readable_book'  => $this->sharedBook->book_id,
+            'modifiable'     => true,
+            'is_owner'       => true,
+            'is_default'     => false,
+        ]);
+        Permission::factory()->create([
+            'permitted_user' => $this->user->id,
+            'readable_book'  => $this->sharedBook->book_id,
+            'modifiable'     => false,
+            'is_owner'       => false,
+            'is_default'     => false,
+        ]);
+        $this->unavailableBook = Book::factory()->create([
+            'book_name' => $this->faker->word(),
+        ]);
+        Permission::factory()->create([
+            'permitted_user' => $this->otherUser->id,
+            'readable_book'  => $this->unavailableBook->book_id,
+            'modifiable'     => true,
+            'is_owner'       => true,
+            'is_default'     => true,
+        ]);
+    }
+
+    public function test_top_page_can_be_diplayed(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get('/page/v1/top/');
+
+        $response->assertOk();
+//        $this->assertTrue(true);
+    }
+
+}
