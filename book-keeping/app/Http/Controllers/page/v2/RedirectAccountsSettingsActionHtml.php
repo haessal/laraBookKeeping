fd7f15f5a31bb9068b2f2d4f34729cc26eca33a9
haessal <haessal@mizutamauki.net>
2023-09-25 22:14:30 +0900

    WIP: fix warning of unknown method

--- book-keeping/app/Http/Controllers/page/v2/RedirectAccountsSettingsActionHtml.php
+++ book-keeping/app/Http/Controllers/page/v2/RedirectAccountsSettingsActionHtml.php
@@ -29,17 +29,20 @@ class RedirectAccountsSettingsActionHtml extends AuthenticatedBookKeepingAction
      */
     public function __invoke(Request $request, string $bookId): RedirectResponse
     {
-        $redirect = redirect()->route('v2_accounts_settings', ['bookId' => $bookId], Response::HTTP_SEE_OTHER);
+        /** @var \Illuminate\Routing\Redirector $redirector */
+        $redirector = redirect();
+
+        $redirect = $redirector->route('v2_accounts_settings', ['bookId' => $bookId], Response::HTTP_SEE_OTHER);
         $accountsgroup = $request->input('accountsgroup');
         if (isset($accountsgroup)) {
             if (strcmp(strval($accountsgroup), '0') != 0) {
-                $redirect = redirect()->route('v2_accounts_groups', ['bookId' => $bookId, 'accountsGroupId' => $accountsgroup], Response::HTTP_SEE_OTHER);
+                $redirect = $redirector->route('v2_accounts_groups', ['bookId' => $bookId, 'accountsGroupId' => $accountsgroup], Response::HTTP_SEE_OTHER);
             }
         }
         $accountsitem = $request->input('accountsitem');
         if (isset($accountsitem)) {
             if (strcmp(strval($accountsitem), '0') != 0) {
-                $redirect = redirect()->route('v2_accounts_items', ['bookId' => $bookId, 'accountsItemId' => $accountsitem], Response::HTTP_SEE_OTHER);
+                $redirect = $redirector->route('v2_accounts_items', ['bookId' => $bookId, 'accountsItemId' => $accountsitem], Response::HTTP_SEE_OTHER);
             }
         }
 
