5364bba7022a843e27030ce47e4b5c953c9c16c4
haessal <haessal@mizutamauki.net>
2023-10-07 19:24:18 +0900

    WIP: modify test for 'BookKeepingService/UpdateAccount*'

--- book-keeping/tests/Unit/Service/BookKeepingService/UpdateAccountGroupTest.php
+++ book-keeping/tests/Unit/Service/BookKeepingService/UpdateAccountGroupTest.php
@@ -28,6 +28,7 @@ class UpdateAccountGroupTest extends TestCase
         $this->be($user);
         $accountGroupId = (string) Str::uuid();
         $newData = ['title' => 'title1755', 'is_current' => true];
+        $result_expected = [BookKeepingService::STATUS_NORMAL, null];
         /** @var \App\Service\BookService|\Mockery\MockInterface $bookMock */
         $bookMock = Mockery::mock(BookService::class);
         $bookMock->shouldReceive('retrieveDefaultBookOrCheckWritable')
@@ -49,8 +50,74 @@ class UpdateAccountGroupTest extends TestCase
         $slipMock = Mockery::mock(SlipService::class);
 
         $BookKeeping = new BookKeepingService($bookMock, $accountMock, $budgetMock, $slipMock);
-        $BookKeeping->updateAccountGroup($accountGroupId, $newData, $bookId);
+        $result_actual = $BookKeeping->updateAccountGroup($accountGroupId, $newData, $bookId);
 
-        $this->assertTrue(true);
+        $this->assertSame($result_expected, $result_actual);
     }
+
+    public function test_it_does_nothing_because_the_specified_account_group_is_not_found_in_the_book(): void
+    {
+        $bookId = (string) Str::uuid();
+        $userId = 61;
+        $user = new User();
+        $user->id = $userId;
+        $this->be($user);
+        $accountGroupId = (string) Str::uuid();
+        $newData = [];
+        $result_expected = [BookKeepingService::STATUS_ERROR_BAD_CONDITION, null];
+        /** @var \App\Service\BookService|\Mockery\MockInterface $bookMock */
+        $bookMock = Mockery::mock(BookService::class);
+        $bookMock->shouldReceive('retrieveDefaultBookOrCheckWritable')
+            ->once()
+            ->with($bookId, $userId)
+            ->andReturn([BookKeepingService::STATUS_NORMAL, $bookId]);
+        /** @var \App\Service\AccountService|\Mockery\MockInterface $accountMock */
+        $accountMock = Mockery::mock(AccountService::class);
+        $accountMock->shouldReceive('retrieveAccountGroups')
+            ->once()
+            ->with($bookId)
+            ->andReturn([]);
+        $accountMock->shouldNotReceive('updateAccountGroup');
+        /** @var \App\Service\BudgetService|\Mockery\MockInterface $budgetMock */
+        $budgetMock = Mockery::mock(BudgetService::class);
+        /** @var \App\Service\SlipService|\Mockery\MockInterface $slipMock */
+        $slipMock = Mockery::mock(SlipService::class);
+
+        $BookKeeping = new BookKeepingService($bookMock, $accountMock, $budgetMock, $slipMock);
+        $result_actual = $BookKeeping->updateAccountGroup($accountGroupId, $newData, $bookId);
+
+        $this->assertSame($result_expected, $result_actual);
+    }
+
+    public function test_it_does_nothing_because_the_specified_book_is_not_writable(): void
+    {
+        $bookId = (string) Str::uuid();
+        $userId = 95;
+        $user = new User();
+        $user->id = $userId;
+        $this->be($user);
+        $accountGroupId = (string) Str::uuid();
+        $newData = [];
+        $result_expected = [BookKeepingService::STATUS_ERROR_AUTH_FORBIDDEN, null];
+        /** @var \App\Service\BookService|\Mockery\MockInterface $bookMock */
+        $bookMock = Mockery::mock(BookService::class);
+        $bookMock->shouldReceive('retrieveDefaultBookOrCheckWritable')
+            ->once()
+            ->with($bookId, $userId)
+            ->andReturn([BookKeepingService::STATUS_ERROR_AUTH_FORBIDDEN, '']);
+        /** @var \App\Service\AccountService|\Mockery\MockInterface $accountMock */
+        $accountMock = Mockery::mock(AccountService::class);
+        $accountMock->shouldNotReceive('retrieveAccountGroups');
+        $accountMock->shouldNotReceive('updateAccountGroup');
+        /** @var \App\Service\BudgetService|\Mockery\MockInterface $budgetMock */
+        $budgetMock = Mockery::mock(BudgetService::class);
+        /** @var \App\Service\SlipService|\Mockery\MockInterface $slipMock */
+        $slipMock = Mockery::mock(SlipService::class);
+
+        $BookKeeping = new BookKeepingService($bookMock, $accountMock, $budgetMock, $slipMock);
+        $result_actual = $BookKeeping->updateAccountGroup($accountGroupId, $newData, $bookId);
+
+        $this->assertSame($result_expected, $result_actual);
+    }
+
 }
