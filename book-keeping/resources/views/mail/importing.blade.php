629775749ba6d72ecc0a0368451325c5e9a3eb1a
haessal <haessal@mizutamauki.net>
2023-11-04 18:13:24 +0900

    WIP: fix warning by prettier

--- book-keeping/resources/views/mail/importing.blade.php
+++ book-keeping/resources/views/mail/importing.blade.php
@@ -5,15 +5,22 @@
     </head>
     <body>
         <p>
-            Refer to the attached file for details of the results. <br/>
+            Refer to the attached file for details of the results.
+            <br />
         </p>
         <table>
             <tbody>
-                <tr><td>sourceUrl:</td><td>{{ $sourceUrl }}</td></tr>
-                <tr><td>status:   </td><td>{{ $status }}   </td></tr>
+                <tr>
+                    <td>sourceUrl:</td>
+                    <td>{{ $sourceUrl }}</td>
+                </tr>
+                <tr>
+                    <td>status:</td>
+                    <td>{{ $status }}</td>
+                </tr>
             </tbody>
         </table>
-        <br/>
+        <br />
         <address>BookKeeping &copy 2007 haessal</address>
     </body>
 </html>
