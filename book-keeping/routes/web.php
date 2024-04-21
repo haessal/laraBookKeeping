74a901f068a2fdda66ef6bbede7d18e8fadb8bb5
haessal <haessal@mizutamauki.net>
2024-04-21 13:53:58 +0900

    Temporarily disable user profile

--- book-keeping/routes/web.php
+++ book-keeping/routes/web.php
@@ -35,12 +35,6 @@ Route::get('/', function () {
 
 Route::get('/dashboard', ShowDashboardActionHtml::class)->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::match(['get', 'post', 'delete'], '/settings/tokens', UpdateAccessTokenActionHtml::class)->middleware(['verified'])->name('settings_tokens');
 
 Route::prefix('/page/v1')->group(function () {
