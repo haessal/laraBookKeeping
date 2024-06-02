20f725d22a519e1808c1723a6c5d142972902baa
haessal <haessal@mizutamauki.net>
2024-06-02 19:52:16 +0900

    WIP: Test supported pages
    Http/Controllers/page/v2/ShowAccountsSettingsActionHtml.php
    Http/Responder/page/v2/ShowAccountsSettingsViewResponder.php

---
+++ book-keeping/tests/Feature/page/v2/books/bookId/accounts/DisplayAccountsSettingsPageTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\page\v2\books\bookId\accounts;
+
+use App\Models\Book;
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DisplayAccountsSettingsPageTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @var \App\Models\User */
+    private $user;
+
+    /** @var \App\Models\User */
+    private $userWhoDoesNotHaveBook;
+
+    /** @var \App\Models\Book */
+    private $book;
+
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->book = Book::factory()->create([
+            'book_name' => $this->faker->word(),
+        ]);
+        Permission::factory()->create([
+            'permitted_user' => $this->user->id,
+            'readable_book' => $this->book->book_id,
+            'modifiable' => true,
+            'is_owner' => true,
+            'is_default' => true,
+        ]);
+        $this->userWhoDoesNotHaveBook = User::factory()->create();
+    }
+
+    public function test_accounts_settings_page_can_be_diplayed(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get('/page/v2/books/'.$this->book->book_id.'/accounts/settings');
+
+        $response->assertOk();
+    }
+
+    public function test_accounts_settings_page_cannot_be_diplayed_because_the_uuid_is_invalid(): void
+    {
+        $response = $this->actingAs($this->user)
+            ->get('/page/v2/books/0/accounts/settings');
+
+        $response->assertNotFound();
+    }
+
+    public function test_the_user_is_not_allowed_to_display_the_accounts_settings_page(): void
+    {
+        $response = $this->actingAs($this->userWhoDoesNotHaveBook)
+            ->get('/page/v2/books/'.$this->book->book_id.'/accounts/settings');
+
+        $response->assertNotFound();
+    }
+}
---
+++ book-keeping/tests/Unit/Http/Controllers/page/v2/ShowAccountsSettingsActionHtml/InvokeTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\page\v2\ShowAccountsSettingsActionHtml;
+
+use App\Http\Controllers\page\v2\ShowAccountsSettingsActionHtml;
+use App\Http\Responder\page\v2\ShowAccountsSettingsViewResponder;
+use App\Service\BookKeepingService;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+use Mockery;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Tests\TestCase;
+
+class InvokeTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function test_it_returns_internal_server_error_with_unexpected_bookkeeping_service_behavior_1(): void
+    {
+        $bookId = (string) Str::uuid();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $serviceMock */
+        $serviceMock = Mockery::mock(BookKeepingService::class);
+        $serviceMock->shouldReceive('validateUuid')
+            ->once()
+            ->with($bookId)
+            ->andReturn(true);
+        $serviceMock->shouldReceive('retrieveBookInformation')
+            ->once()
+            ->with($bookId)
+            ->andReturn([BookKeepingService::STATUS_NORMAL, null]);
+        $serviceMock->shouldNotReceive('retrieveCategorizedAccounts');
+        /** @var \App\Http\Responder\page\v2\ShowAccountsSettingsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowAccountsSettingsViewResponder::class);
+        $responderMock->shouldNotReceive('response');
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+
+        $controller = new ShowAccountsSettingsActionHtml($serviceMock, $responderMock);
+        try {
+            $response = $controller->__invoke($requestMock, $bookId);
+            $this->fail('Expected HttpException was not thrown');
+        } catch (HttpException $e) {
+            $this->assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getStatusCode());
+        }
+    }
+
+    public function test_it_returns_internal_server_error_with_unexpected_bookkeeping_service_behavior_2(): void
+    {
+        $bookId = (string) Str::uuid();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $serviceMock */
+        $serviceMock = Mockery::mock(BookKeepingService::class);
+        $serviceMock->shouldReceive('validateUuid')
+            ->once()
+            ->with($bookId)
+            ->andReturn(true);
+        $serviceMock->shouldReceive('retrieveBookInformation')
+            ->once()
+            ->with($bookId)
+            ->andReturn([-1, null]);
+        $serviceMock->shouldNotReceive('retrieveCategorizedAccounts');
+        /** @var \App\Http\Responder\page\v2\ShowAccountsSettingsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowAccountsSettingsViewResponder::class);
+        $responderMock->shouldNotReceive('response');
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+
+        $controller = new ShowAccountsSettingsActionHtml($serviceMock, $responderMock);
+        try {
+            $response = $controller->__invoke($requestMock, $bookId);
+            $this->fail('Expected HttpException was not thrown');
+        } catch (HttpException $e) {
+            $this->assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getStatusCode());
+        }
+    }
+
+    public function test_it_returns_internal_server_error_with_unexpected_bookkeeping_service_behavior_3(): void
+    {
+        $bookId = (string) Str::uuid();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $serviceMock */
+        $serviceMock = Mockery::mock(BookKeepingService::class);
+        $serviceMock->shouldReceive('validateUuid')
+            ->once()
+            ->with($bookId)
+            ->andReturn(true);
+        $serviceMock->shouldReceive('retrieveBookInformation')
+            ->once()
+            ->with($bookId)
+            ->andReturn([BookKeepingService::STATUS_NORMAL, []]);
+        $serviceMock->shouldReceive('retrieveCategorizedAccounts')
+            ->once()
+            ->with(false, $bookId)
+            ->andReturn([BookKeepingService::STATUS_NORMAL, null]);
+        /** @var \App\Http\Responder\page\v2\ShowAccountsSettingsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowAccountsSettingsViewResponder::class);
+        $responderMock->shouldNotReceive('response');
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+
+        $controller = new ShowAccountsSettingsActionHtml($serviceMock, $responderMock);
+        try {
+            $response = $controller->__invoke($requestMock, $bookId);
+            $this->fail('Expected HttpException was not thrown');
+        } catch (HttpException $e) {
+            $this->assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getStatusCode());
+        }
+    }
+
+    public function test_it_returns_internal_server_error_with_unexpected_bookkeeping_service_behavior_4(): void
+    {
+        $bookId = (string) Str::uuid();
+        /** @var \App\Service\BookKeepingService|\Mockery\MockInterface $serviceMock */
+        $serviceMock = Mockery::mock(BookKeepingService::class);
+        $serviceMock->shouldReceive('validateUuid')
+            ->once()
+            ->with($bookId)
+            ->andReturn(true);
+        $serviceMock->shouldReceive('retrieveBookInformation')
+            ->once()
+            ->with($bookId)
+            ->andReturn([BookKeepingService::STATUS_NORMAL, []]);
+        $serviceMock->shouldReceive('retrieveCategorizedAccounts')
+            ->once()
+            ->with(false, $bookId)
+            ->andReturn([-1, null]);
+        /** @var \App\Http\Responder\page\v2\ShowAccountsSettingsViewResponder|\Mockery\MockInterface $responderMock */
+        $responderMock = Mockery::mock(ShowAccountsSettingsViewResponder::class);
+        $responderMock->shouldNotReceive('response');
+        /** @var \Illuminate\Http\Request|\Mockery\MockInterface $requestMock */
+        $requestMock = Mockery::mock(Request::class);
+
+        $controller = new ShowAccountsSettingsActionHtml($serviceMock, $responderMock);
+        try {
+            $response = $controller->__invoke($requestMock, $bookId);
+            $this->fail('Expected HttpException was not thrown');
+        } catch (HttpException $e) {
+            $this->assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getStatusCode());
+        }
+    }
+}
