df95dfe25424a3c50c43269a8df7e3b060954678
haessal <haessal@mizutamauki.net>
2024-06-29 16:52:56 +0900

    Treat CreditCardStatement as a BookKeeping model and associate to DB table

--- book-keeping/app/Models/CreditCardStatement.php
+++ book-keeping/app/Models/CreditCardStatement.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class CreditCardStatement extends Model
+/**
+ * App\Models\CreditCardStatement.
+ *
+ * @property string $credit_card_statement_id
+ * @property string $book_id
+ * @property string $credit_card_statement_outline
+ * @property string|null $credit_card_statement_memo
+ * @property string $date
+ * @property int|null $display_order
+ */
+class CreditCardStatement extends BookKeepingBasicModel
 {
-    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_credit_card_statements';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'credit_card_statement_id';
 }
