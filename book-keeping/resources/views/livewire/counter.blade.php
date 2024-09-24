94550bf0d631e1941a86811b0ba56f2d09708290
haessal <haessal@mizutamauki.net>
2024-09-24 14:12:29 +0900

    try livewire counter in sandbox

---
+++ book-keeping/resources/views/livewire/counter.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">{{ $count }}</h1>
+    <button class="font-semibold text-xl text-gray-800 dark:text-gray-200" wire:click="increment">+</button>
+</div>
--- book-keeping/resources/views/sandbox/show.blade.php
+++ book-keeping/resources/views/sandbox/show.blade.php
@@ -40,21 +40,7 @@
                     <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                         {{ __("Try counter component of LiveWire.") }}
                     </p>
-                    <form method="POST" action="{{ route('sandbox.show') }}">
-                        @csrf
-                        <div class="mt-4">
-                        <input
-                                datepicker
-                                datepicker-autohide
-                                datepicker-format="yyyy-mm-dd"
-                                type="text"
-                                id="sandbox-date"
-                                name="sandbox-date"
-                                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-                                placeholder="Select date"
-                                readonly />
-                        </div>
-                    </form>
+                    <livewire:counter />
                 </div>
             </div>
         </div>
