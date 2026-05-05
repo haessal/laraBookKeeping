0d9fb57f601a4c21500d4ad9283625778fd8fe63
haessal <haessal@mizutamauki.net>
2026-05-05 14:59:42 +0900

    Generate code reference and bundle it with the release archive

--- scripts/create-release-for-xrea.sh
+++ scripts/create-release-for-xrea.sh
@@ -13,17 +13,31 @@ echo "Building release: ${VERSION}"
 echo "Building assets..."
 npm run build
 
+# Generate code reference
+echo "Generating code reference..."
+rm -Rf docs
+rm -Rf .phpdoc
+phpdoc -d ./app -t ./docs --ignore="vendor/*,node_modules/*,tests/*"
+
 cd ..
 
 # Create release archive
 echo "Creating archive: ${ARCHIVE_NAME}"
 tar -czf "${ARCHIVE_NAME}" \
-    --exclude='book-keeping/.git' \
+    --exclude='book-keeping/.DS_Store' \
     --exclude='book-keeping/.env' \
+    --exclude='book-keeping/.gitattributes' \
+    --exclude='book-keeping/.gitignore' \
+    --exclude='book-keeping/.phpdoc' \
+    --exclude='book-keeping/.phpunit.result.cache' \
+    --exclude='book-keeping/.phpunit.cache' \
+    --exclude='book-keeping/.prettierignore' \
+    --exclude='book-keeping/.prettierrc' \
     --exclude='book-keeping/node_modules' \
-    --exclude='book-keeping/vendor' \
-    --exclude='book-keeping/tests' \
+    --exclude='book-keeping/phpstan.neon' \
     --exclude='book-keeping/phpunit.xml' \
+    --exclude='book-keeping/tests' \
+    --exclude='book-keeping/vendor' \
     book-keeping/
 
 mv "${ARCHIVE_NAME}" ./deploy/xrea/
