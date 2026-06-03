14c2eee11ff1a35a1517493b047e035554d16d58
haessal <haessal@mizutamauki.net>
2026-06-03 21:30:53 +0900

    Make Eloquent models

---
+++ book-keeping/app/Models/DataProvider/Eloquent/SlipGroupEntry.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\DataProvider\Eloquent;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SlipGroupEntry extends Model
+{
+    /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\SlipGroupEntryFactory> */
+    use HasFactory;
+}
