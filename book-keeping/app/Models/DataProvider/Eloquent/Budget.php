64e98df46d3ba728762c480293848035e9d20d97
haessal <haessal@mizutamauki.net>
2026-06-03 21:30:53 +0900

    Make Eloquent models

---
+++ book-keeping/app/Models/DataProvider/Eloquent/Budget.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\DataProvider\Eloquent;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Budget extends Model
+{
+    /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\BudgetFactory> */
+    use HasFactory;
+}
