b303abd90ab7d19d0b955e10a6069acd89b197e6
haessal <haessal@mizutamauki.net>
2026-06-06 13:25:27 +0900

    Make factories for Eloquent models

---
+++ book-keeping/database/factories/DataProvider/Eloquent/AccountGroupFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\DataProvider\Eloquent;
+
+use App\Models\DataProvider\Eloquent\AccountGroup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<AccountGroup>
+ */
+class AccountGroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ book-keeping/database/factories/DataProvider/Eloquent/BookFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\DataProvider\Eloquent;
+
+use App\Models\DataProvider\Eloquent\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
