25c7f916f9c4bac87c90b5a6cc22bda440d8cc4c
haessal <haessal@mizutamauki.net>
2026-06-06 13:25:27 +0900

    Make factories for Eloquent models

---
+++ book-keeping/database/factories/DataProvider/Eloquent/SlipFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\DataProvider\Eloquent;
+
+use App\Models\DataProvider\Eloquent\Slip;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Slip>
+ */
+class SlipFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
