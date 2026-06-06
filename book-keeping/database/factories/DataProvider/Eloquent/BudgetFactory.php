479613317fdcb54ec40411a1fa1ee615a38b6450
haessal <haessal@mizutamauki.net>
2026-06-06 13:25:27 +0900

    Make Eloquent models with factories

---
+++ book-keeping/database/factories/DataProvider/Eloquent/BudgetFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories\DataProvider\Eloquent;
+
+use App\Models\DataProvider\Eloquent\Budget;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Budget>
+ */
+class BudgetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
