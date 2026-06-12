00e8247a090e54efaa95187b231c6d3852631487
haessal <haessal@mizutamauki.net>
2026-06-12 19:50:42 +0900

    Setup BookKeeping models
    For BookKeeping models:
          - Associate the models to DB table
          - Generate UUID for the primary of a new record
          - Treate delete as soft delete

--- book-keeping/app/Models/DataProvider/Eloquent/Budget.php
+++ book-keeping/app/Models/DataProvider/Eloquent/Budget.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Budget extends Model
+#[Fillable(['book_id', 'account_code', 'date', 'amount'])]
+class Budget extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\BudgetFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_budgets';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'budget_id';
 }
--- book-keeping/app/Models/DataProvider/Eloquent/SlipGroupEntry.php
+++ book-keeping/app/Models/DataProvider/Eloquent/SlipGroupEntry.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class SlipGroupEntry extends Model
+#[Fillable(['slip_group_id', 'related_slip'])]
+class SlipGroupEntry extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\SlipGroupEntryFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_slip_group_entries';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'slip_group_entry_id';
 }
