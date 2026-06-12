66de1efde5919356a8d32d65b4554a42d6279ffa
haessal <haessal@mizutamauki.net>
2026-06-12 19:50:42 +0900

    Setup BookKeeping models
    For BookKeeping models:
          - Associate the models to DB table
          - Generate UUID for the primary of a new record
          - Treate delete as soft delete

--- book-keeping/app/Models/DataProvider/Eloquent/Permission.php
+++ book-keeping/app/Models/DataProvider/Eloquent/Permission.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
+#[Fillable(['permitted_user', 'readable_book', 'modifiable', 'is_owner', 'is_default'])]
+class Permission extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\PermissionFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_permissions';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'permission_id';
 }
--- book-keeping/app/Models/DataProvider/Eloquent/SlipEntry.php
+++ book-keeping/app/Models/DataProvider/Eloquent/SlipEntry.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class SlipEntry extends Model
+#[Fillable(['slip_id', 'debit', 'credit', 'amount', 'client', 'outline', 'credit_card_statement_id'])]
+class SlipEntry extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\SlipEntryFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_slip_entries';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'slip_entry_id';
 }
