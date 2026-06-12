306f841b32dcddb61e8d352490144087972085dc
haessal <haessal@mizutamauki.net>
2026-06-12 20:03:03 +0900

    Add has-many relationships to the models

--- book-keeping/app/Models/DataProvider/Eloquent/SlipGroup.php
+++ book-keeping/app/Models/DataProvider/Eloquent/SlipGroup.php
@@ -4,6 +4,7 @@ namespace App\Models\DataProvider\Eloquent;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['book_id', 'slip_group_outline', 'slip_group_memo'])]
 class SlipGroup extends BookKeepingBasicModel
@@ -24,4 +25,14 @@ class SlipGroup extends BookKeepingBasicModel
      * @var string
      */
     protected $primaryKey = 'slip_group_id';
+
+    /**
+     * Get the slip group entries for the slip group.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<SlipGroupEntry, $this>
+     */
+    public function slipGroupEntries(): HasMany
+    {
+        return $this->hasMany(SlipGroupEntry::class, 'slip_group_id', 'slip_group_id');
+    }
 }
