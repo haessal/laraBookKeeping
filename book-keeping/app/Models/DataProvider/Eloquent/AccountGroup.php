34fb043041bfb2cd51741f55becc4368bfd38baa
haessal <haessal@mizutamauki.net>
2026-06-12 19:50:42 +0900

    Setup BookKeeping models
    For BookKeeping models:
          - Associate the models to DB table
          - Generate UUID for the primary of a new record
          - Treate delete as soft delete

--- book-keeping/app/Models/DataProvider/Eloquent/AccountGroup.php
+++ book-keeping/app/Models/DataProvider/Eloquent/AccountGroup.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class AccountGroup extends Model
+#[Fillable(['book_id', 'account_type', 'account_group_title', 'bk_uid', 'account_group_bk_code', 'is_current'])]
+class AccountGroup extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\AccountGroupFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_account_groups';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'account_group_id';
 }
--- book-keeping/app/Models/DataProvider/Eloquent/SlipGroup.php
+++ book-keeping/app/Models/DataProvider/Eloquent/SlipGroup.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class SlipGroup extends Model
+#[Fillable(['book_id', 'slip_group_outline', 'slip_group_memo'])]
+class SlipGroup extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\SlipGroupFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_slip_groups';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'slip_group_id';
 }
