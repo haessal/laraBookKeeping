458b77b5367418ad480dd7f10d244e36fc378662
haessal <haessal@mizutamauki.net>
2026-06-12 19:50:42 +0900

    Setup BookKeeping models
    For BookKeeping models:
          - Associate the models to DB table
          - Generate UUID for the primary of a new record
          - Treate delete as soft delete

--- book-keeping/app/Models/DataProvider/Eloquent/Account.php
+++ book-keeping/app/Models/DataProvider/Eloquent/Account.php
@@ -2,11 +2,26 @@
 
 namespace App\Models\DataProvider\Eloquent;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Account extends Model
+#[Fillable(['account_group_id', 'account_title', 'description', 'selectable', 'is_credit_card', 'bk_uid', 'account_bk_code'])]
+class Account extends BookKeepingBasicModel
 {
     /** @use HasFactory<\Database\Factories\DataProvider\Eloquent\AccountFactory> */
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bk2_0_accounts';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'account_id';
 }
