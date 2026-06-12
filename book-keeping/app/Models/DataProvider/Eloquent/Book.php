0bedff2e3eba8ed3634c16e097bb1e8ad8a4758c
haessal <haessal@mizutamauki.net>
2026-06-12 20:03:03 +0900

    Add has-many relationships to the models

--- book-keeping/app/Models/DataProvider/Eloquent/Book.php
+++ book-keeping/app/Models/DataProvider/Eloquent/Book.php
@@ -4,6 +4,7 @@ namespace App\Models\DataProvider\Eloquent;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['book_name'])]
 class Book extends BookKeepingBasicModel
@@ -24,4 +25,54 @@ class Book extends BookKeepingBasicModel
      * @var string
      */
     protected $primaryKey = 'book_id';
+
+    /**
+     * Get the account groups for the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<AccountGroup, $this>
+     */
+    public function accountGroups(): HasMany
+    {
+        return $this->hasMany(AccountGroup::class, 'book_id', 'book_id');
+    }
+
+    /**
+     * Get the slips for the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Slip, $this>
+     */
+    public function slips(): HasMany
+    {
+        return $this->hasMany(Slip::class, 'book_id', 'book_id');
+    }
+
+    /**
+     * Get the credit card statements for the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<CreditCardStatement, $this>
+     */
+    public function creditCardStatements(): HasMany
+    {
+        return $this->hasMany(CreditCardStatement::class, 'book_id', 'book_id');
+    }
+
+    /**
+     * Get the budgets for the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Budget, $this>
+     */
+    public function budgets(): HasMany
+    {
+        return $this->hasMany(Budget::class, 'book_id', 'book_id');
+    }
+
+    /**
+     * Get the slip groups for the book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<SlipGroup, $this>
+     */
+    public function slipGroups(): HasMany
+    {
+        return $this->hasMany(SlipGroup::class, 'book_id', 'book_id');
+    }
 }
